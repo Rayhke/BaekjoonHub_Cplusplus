d155a7ea941e8d3a2e6fddc7bdd38dcaf3008d9a
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-08 01:12:54 +0900

    Update A＋B.cc

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,9 +1,9 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d", a + b);
+	cin >> a >> b;
+	cout << a + b;
 	return 0;
 }
