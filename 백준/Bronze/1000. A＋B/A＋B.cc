e4006d43664fce0f718a2199afb36dd987ddc708
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-08 01:11:44 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cc
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 int main() {
 	int a, b;
-	cin >> a >> b;
-	cout << a + b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a + b);
 	return 0;
 }
