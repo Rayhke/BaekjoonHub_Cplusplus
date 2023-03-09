e037d3c7ddd776ab6d590278ea31bffa04ce76f5
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-09 19:31:16 +0900

    Update A－B.cc

--- 백준/Bronze/1001. A－B/A－B.cc
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -7,3 +7,16 @@ int main() {
 	cout << a - b;
 	return 0;
 }
+/*========================================*/
+/* ■ C언어 ■
+
+  #define _CRT_SECURE_NO_WARNINGS
+  #include <stdio.h>
+
+  int main() {
+  	int a, b;
+  	scanf("%d %d", &a, &b);
+  	printf("%d", a - b);
+  	return 0;
+  }
+*/
