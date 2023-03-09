10a6e42411178025aaf08d4bc386b2fefc095914
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-09 23:00:02 +0900

    Update 두 수 비교하기.cc

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -1,19 +1,29 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main() {
 	int a, b;
-	scanf("%d %d", &a, &b);
-
-	if (a > b) {
-		printf(">");
-	}
-	if (a < b) {
-		printf("<");
-	}
-	if (a == b) {
-		printf("==");
-	}
-
+	cin >> a >> b;
+	if (a > b) {cout << ">";}
+	else if (a < b) {cout << "<";}
+	else {cout << "==";}
 	return 0;
 }
+/* ■ 설명 ■
+조건문 [if()]
+*/
+/*========================================*/
+/* ■ C언어 ■
+
+  #define _CRT_SECURE_NO_WARNINGS
+  #include <stdio.h>
+
+  int main() {
+  	int a, b;
+  	scanf("%d %d", &a, &b);
+  	if (a > b) {printf(">");}
+  	else if (a < b) {printf("<");}
+  	else {printf("==");}
+  	return 0;
+	}
+*/
