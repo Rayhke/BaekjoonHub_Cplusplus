51326d6d2645b45b98ceba67d2191e7d5d98f920
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-25 21:13:14 +0900

    Update A＋B － 5.cc

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -2,15 +2,18 @@
 using namespace std;
 
 int main() {
-	int a = 0, b = 0, n = 0, m, add = 1;
+	int a = 0, b = 0, n, m, add = 1;
 	int num[100000] = { 0 };
-	while (1) {
+	for (n = 0; add > 0;) {
 		cin >> a >> b;
 		num[n] = a + b;
-		if (a == 0 && b == 0) {
-			break;
+		if (a > 0 && b > 0) {
+			add = 1;
+            		n++;
 		}
-		n++;
+		else {
+			add = 0;
+		}
 	}
 	for (m = 0; m < n; m++) {
 		cout << num[m] << "\n";
