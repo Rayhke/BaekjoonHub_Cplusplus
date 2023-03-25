dedae31f5007d2e4933dd28317f41842b7924c53
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-25 21:12:08 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -2,18 +2,15 @@
 using namespace std;
 
 int main() {
-	int a = 0, b = 0, n, m, add = 1;
+	int a = 0, b = 0, n = 0, m, add = 1;
 	int num[100000] = { 0 };
-	for (n = 0; add > 0;) {
+	while (1) {
 		cin >> a >> b;
 		num[n] = a + b;
-		if (a > 0 && b > 0) {
-			add = 1;
-			n++;
+		if (a == 0 && b == 0) {
+			break;
 		}
-		else {
-			add = 0;
-		}
+		n++;
 	}
 	for (m = 0; m < n; m++) {
 		cout << num[m] << "\n";
