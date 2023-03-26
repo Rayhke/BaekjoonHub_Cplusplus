af7b54072dc27caca44f1806e4b5257c72b52491
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-26 17:39:39 +0900

    Update A×B.cc

--- 백준/Bronze/10998. A×B/A×B.cc
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -8,15 +8,14 @@ int main() {
 	return 0;
 }
 /*========================================*/
-/* ■ C언어 ■
+/* ■ C언어 ■ */
 
-  #define _CRT_SECURE_NO_WARNINGS
-  #include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-  int main() {
-  	int a, b;
-  	scanf("%d %d", &a, &b);
-  	printf("%d", a * b);
-  	return 0;
-  }
-*/
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a * b);
+	return 0;
+}
