ebca4704734c74beafbc044846d6b3ab8bd86f41
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-26 14:36:18 +0900

    Update We love kriii.cc

--- 백준/Bronze/10718. We love kriii/We love kriii.cc
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -1,2 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+         int 
+}
+
+/*===========================================================*/
+/* ■ C언어 ■ */
+
 #include <stdio.h>
-int main(){printf("강한친구 대한육군\n강한친구 대한육군");}
+int main() {
+          printf("강한친구 대한육군\n강한친구 대한육군");
+}
