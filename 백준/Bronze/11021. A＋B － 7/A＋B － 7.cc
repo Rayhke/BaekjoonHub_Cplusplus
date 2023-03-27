c27503c2781ecf6e35f0939f47bed2e3bf3ee7b3
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-03-27 19:54:01 +0900

    Update A＋B － 7.cc

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -18,3 +18,26 @@ int main() {
 	}
 	return 0;
 }
+/*===========================================================*/
+/* ■ C 언어 ■ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, t;
+	int c[100000] = { 0 };
+	scanf("%d", &t);
+	for (int n = 0; n < t; n++) {
+		scanf("%d %d", &a, &b);
+		c[n] = a + b;
+	}
+	for (int m = 0; m < t;) {
+		printf("Case #%d: %d", m + 1, c[m]);
+		m++;
+		if (m < t) {
+			printf("\n");
+		}
+	}
+	return 0;
+}
