ab5e4f5dc027a4a6a5af2153ba811fbece72a0d5
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-01 21:50:39 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char a[1000];
+	char b[1000];
+	cin >> a >> b;
+	if (strlen(a) >= strlen(b)) cout << "go";
+	else cout << "no";
+	return 0;
+}
