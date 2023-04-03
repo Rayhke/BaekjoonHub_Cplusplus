dbc7608730d93071c37dce5924a7635c8c04cfbc
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-03 13:15:54 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, d = 0;
+	cin >> a >> b >> c;
+	if (a == b && b == c) {
+		d = 10000 + (a * 1000);
+	}
+	else if (a == b || a == c || b == c) {
+		if (a == b || a == c) {
+			d = 1000 + (a * 100);
+		}
+		else if (b == c) {
+			d = 1000 + (b * 100);
+		}
+	}
+	else if (a != b && a != c && b != c) {
+		if (a > b && a > c) {
+			d = a * 100;
+		}
+		else if (b > a && b > c) {
+			d = b * 100;
+		}
+		else if (c > a && c > b) {
+			d = c * 100;
+		}
+	}
+	cout << d;
+	return 0;
+}
