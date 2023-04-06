420fc5eca879ce2305c6306cb7da0454e65b81fe
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-07 00:59:54 +0900

    Update 직사각형.cc

--- 백준/Bronze/27323. 직사각형/직사각형.cc
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -7,3 +7,17 @@ int main() {
 	cout << a * b;
 	return 0;
 }
+/* ■ 설명 ■
+[직사각형의 넓이 cm² = 직사각형의 가로 (a) × 직사각형의 세로 (b)]
+이므로 가로(a)와 세로(b)에 값을 [cin] 입력하여 [cout] 출력시 곱하기.
+*/
+/* ■ C언어 ■*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d", a * b);
+	return 0;
+}
