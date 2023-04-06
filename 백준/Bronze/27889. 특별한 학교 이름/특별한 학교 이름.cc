00a9d170657d042011de8ebc0f31504dfd4b39cc
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-07 00:35:16 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string n;
+	cin >> n;
+	if (n == "NLCS") cout << "North London Collegiate School";
+	if (n == "BHA")cout << "Branksome Hall Asia";
+	if (n == "KIS")cout << "Korea International School";
+	if (n == "SJA")cout << "St. Johnsbury Academy";
+	return 0;
+}
