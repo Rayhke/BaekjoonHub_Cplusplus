053d5b2997a686473a9f2c924db4f54b0acfd350
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-07 17:38:52 +0900

    Update 조별과제를 하려는데 조장이 사라졌다.cc

--- 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -11,3 +11,23 @@ int main() {
 	cout << n;
 	return 0;
 }
+/* ■ 설명 ■
+
+
+*/
+/*==============================*/
+/* ■ C언어 ■ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, n = 0;
+	scanf("%d", &a);
+	for (; a > 0;) {
+		a -= 5;
+		n++;
+	}
+	printf("%d", n);
+	return 0;
+}
