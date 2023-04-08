cb52604fa12ef89e9b75d2741b2e85ef739cc6ae
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-08 23:25:18 +0900

    Update 주사위 세개.cc

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -8,23 +8,13 @@ int main() {
 		d = 10000 + (a * 1000);
 	}
 	else if (a == b || a == c || b == c) {
-		if (a == b || a == c) {
-			d = 1000 + (a * 100);
-		}
-		else if (b == c) {
-			d = 1000 + (b * 100);
-		}
+		if (a == b || a == c) d = 1000 + (a * 100); 
+		else if (b == c) d = 1000 + (b * 100); 
 	}
 	else if (a != b && a != c && b != c) {
-		if (a > b && a > c) {
-			d = a * 100;
-		}
-		else if (b > a && b > c) {
-			d = b * 100;
-		}
-		else if (c > a && c > b) {
-			d = c * 100;
-		}
+		if (a > b && a > c) d = a * 100;
+		else if (b > a && b > c) d = b * 100;
+		else if (c > a && c > b) d = c * 100; 
 	}
 	cout << d;
 	return 0;
