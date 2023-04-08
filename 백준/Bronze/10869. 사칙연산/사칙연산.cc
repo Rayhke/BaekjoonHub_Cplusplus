4f877dee6005d0032bf8004549371eede78c593d
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-08 21:57:37 +0900

    Update 사칙연산.cc

--- 백준/Bronze/10869. 사칙연산/사칙연산.cc
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -1,2 +1,15 @@
-#include<stdio.h>
-int main(){int a,b;scanf("%d %d",&a,&b);printf("%d\n%d\n%d\n%d\n%d",a+b,a-b,a*b,a/b,a%b);}
+#include <iostream>
+using namespace std;
+
+int Add(int a, int b) { return a + b; }
+int Sub(int a, int b) { return a - b; }
+int Mul(int a, int b) { return a * b; }
+int Div(int a, int b) { return a / b; }
+int Rem(int a, int b) { return a % b; }
+
+int main(int argc, char* argv[]) {
+	int a, b;
+	cin >> a >> b;
+	cout << Add(a, b) << "\n" << Sub(a, b) << "\n" << Mul(a, b) << "\n" << Div(a, b) << "\n" << Rem(a, b);
+	return 0;
+}
