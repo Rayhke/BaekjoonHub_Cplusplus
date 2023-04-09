c09b74bb6477891a7d59929aa4aa58c24e7afb76
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-09 14:45:25 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long a, b = 1;
+	cin >> a;
+	for (int n = 1; n <= a; n++) {
+		b = b * n;
+	}
+	if (b > 0) {
+		cout << b;
+	}
+	else {
+		cout << "1";
+	}
+	return 0;
+}
