54f56a112aa7b6cd0cea23d4280bdbd3abac851f
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-09 14:47:45 +0900

    Update 팩토리얼 2.cc

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -4,14 +4,8 @@ using namespace std;
 int main() {
 	long long a, b = 1;
 	cin >> a;
-	for (int n = 1; n <= a; n++) {
-		b = b * n;
-	}
-	if (b > 0) {
-		cout << b;
-	}
-	else {
-		cout << "1";
-	}
+	for (int n = 1; n <= a; n++) b = b * n;
+	if (b > 0) cout << b;
+	else cout << "1";
 	return 0;
 }
