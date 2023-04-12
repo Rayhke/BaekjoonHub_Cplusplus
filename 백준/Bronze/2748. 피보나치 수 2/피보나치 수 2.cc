56893284883d5f3b151320777b6b522bbfe0b759
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-12 10:05:23 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m = 0;
+	long long a[91];
+	a[0] = 0; a[1] = 1;
+	cin >> n;
+	if (n < 2) {
+		m = n;
+	}
+	else if (n > 1) {
+		for (int l = 2; l <= n; l++) {
+			a[l] = a[l - 1] + a[l - 2];
+			m = l;
+		}
+	}
+	cout << a[m];
+	return 0;
+}
