13dc8401b2e9c407e6d04ec77a9abd1c580dd63c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-14 12:23:09 +0900

    Update 별 찍기 － 1.cc

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -8,7 +8,7 @@ int main() {
 		for (int b = 0; b <= a; b++) {
 			cout << "*";
 		}
-		if (a != (n - 1))cout << "\n";
+		if (a != (n - 1))cout << "\n"; // 마지막 출력 줄은 줄바꿈 생략하도록, 절차를 추가
 	}
 	return 0;
 }
