9b30091c4b1ae2d8de6a9766d9e19902e3b0f31b
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-14 13:50:45 +0900

    Update OX퀴즈.cc

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.cc
@@ -8,21 +8,25 @@ int main() {
 	cin >> N;
 	for (int m = 0; m < N; m++) {
 		cin >> s;
-		while (1) {
+		while (1) {						// ' M ' 번째 퀴즈 전체 점수를 계산하는 시스템
 			if (s[M] == 'O') {
 				p++;
 				if (M != 0 && s[M - 1] == 'O') {
 					q++; p += q; 
 				}
-				else q = 0;
+				else {
+					q = 0;
+				}
 			}
-			else if (s[M] == NULL) break;
+			else if (s[M] == NULL) {
+				break;
+			}
 			M++;
 		}
-		S[m] = p;
-		s = { 0 };
-		M = 0;
-		p = 0;
+		S[m] = p;						// ' M ' 번째 퀴즈 총합 점수를 ' M ' 번째에 기입
+		s = { 0 };						// ' M ' 번째 퀴즈 문제 답안지 값 초기화
+		M = 0;							// ' M ' 번째 퀴즈 문제 답안지 번호 값 초기화
+		p = 0;							// ' M ' 번째 퀴즈 총합 점수 초기화
 	}
 	for (int l = 0; l < N; l++) {
 		cout << S[l];
