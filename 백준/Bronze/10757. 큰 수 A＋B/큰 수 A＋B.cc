f2fbca0a527276bbca41f8a19a45a40db2108358
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-15 17:32:44 +0900

    Update 큰 수 A＋B.cc

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -2,10 +2,17 @@
 #include <string>
 using namespace std;
 
-string s(string a, string b) {
-	if (a.size() < b.size()) swap(a, b);
+string BigSum(string a, string b) {	// 입력 받은 값을 맨 한 칸, 한 칸 씩 덧셈 연산 작업
+	if (a.size() < b.size()) {	// 그 후, 한 칸 자리 값이 ' 0 ~ 9 ' 를 넘는 ' 10 ' 이 된다면?
+		swap(a, b);		// 앞 칸에 ' + 1 ' 을 처리하는 작업
+	}
+
 	int m = a.size() - 1;
-	for (int n = b.size() - 1; n >= 0; n--, m--) a[m] += (b[n] - '0');
+
+	for (int n = b.size() - 1; n >= 0; n--, m--) {
+		a[m] += (b[n] - '0');
+	}
+
 	for (int n = a.size() - 1; n > 0; n--) {
 		if (a[n] > '9') {
 			int c = a[n] - '0';
@@ -13,6 +20,7 @@ string s(string a, string b) {
 			a[n] = (c % 10) + '0';
 		}
 	}
+
 	if (a[0] > '9') {
 		string d;
 		d += a[0];
@@ -20,12 +28,16 @@ string s(string a, string b) {
 		d[0] = ((d[0] - '0') / 10) + '0';
 		a = d + a;
 	}
+
 	return a;
 }
 
 int main() {
 	string a, b;
 	cin >> a >> b;
-	cout << s(a, b);
+	cout << BigSum(a, b);
 	return 0;
 }
+/* ■ 설명 ■
+
+*/
