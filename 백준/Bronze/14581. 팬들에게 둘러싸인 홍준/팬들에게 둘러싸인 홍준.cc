d7eaeff5fa9cd2a17c7a64a01d80d8d327b21101
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-18 14:24:17 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a[21];
+	cin >> a;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << a << "::fan:\n";
+	cout << ":fan::fan::fan:";
+	return 0;
+}
