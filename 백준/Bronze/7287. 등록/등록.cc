6844c8b0690e2c0cbd079b60cff5eaab189caa14
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-22 17:52:01 +0900

    Update 등록.cc

--- 백준/Bronze/7287. 등록/등록.cc
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << 133 << std::endl;
-	std::cout << "nink2458" << std::endl;
+	std::cout << 133 << std::endl;		// 백준에서 문제 풀은 갯수 출력
+	std::cout << "nink2458" << std::endl;	// 백준 아이디를 출력
 	return 0;
 }
