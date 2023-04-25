0152c9d23316e4baeb792ef738c24874ddbe05d0
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-25 10:16:35 +0900

    Update 초콜릿 자르기.cc

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main() {
-	int N, M;
-	cin >> N >> M;
-	cout << ((N * M) - 1) << endl;
+	int N, M;				// 가로, 세로 입력
+	cin >> N >> M;				
+	cout << ((N * M) - 1) << endl;		// 
 	return 0;
 }
+/* ■ 설명 ■
+초콜릿을 한 조각에 한 번씩, 쪼개는 횟수
+
+*/
