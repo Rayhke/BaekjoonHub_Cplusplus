596ca255c864d9a5c24e1c12504d8cecb5fbb6e4
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-27 13:54:14 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int m = 0; m < n; m++) {
+		for (int l = 0; l <= m; l++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	while ((n - 1) > 0) {
+		for (int m = 0; m < (n - 1); m++) {
+			cout << "*";
+		}
+		cout << endl;
+		n--;
+	}
+	return 0;
+}
