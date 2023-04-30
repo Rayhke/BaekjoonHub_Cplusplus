68757ca7ea01737485b8c379324c18b0b0d3edee
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-04-30 17:45:23 +0900

    Update Who is in the middle？.cc

--- 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cc
@@ -4,12 +4,17 @@ using namespace std;
 int main() {
 	int a, b, c, d, e, f;
 	cin >> a >> b >> c;
-	if (a == b && a == c && b == c) { cout << a; }
+	if (a == b && a == c && b == c) { cout << a; }		// 만약 a, b, c 값이 전부 동일 하다면, 하나 출력 
 	else {
-		f = a + b + c;
-		d = (a < b) ? a : b; d = (d < c) ? d : c;
-		e = (a > b) ? a : b; e = (e > c) ? e : c;
+		f = a + b + c;					// a + b + c 값 전부 더하기
+		d = (a < b) ? a : b; d = (d < c) ? d : c;	// 가장 큰 값 찾기
+		e = (a > b) ? a : b; e = (e > c) ? e : c;	// 가장 작은 값 찾기
 		cout << (f - (d + e)) << endl;
 	}
 	return 0;
 }
+/* ■ 설명 ■	 // 백준 - 브론즈 5 (6840번)
+제일 작은 그릇의 무게 순으로
+아기 곰, 엄마 곰, 아빠 곰 그릇이라고 한다면
+즉, 3개의 입력 값 중 중간 값을 찾아라
+*/
