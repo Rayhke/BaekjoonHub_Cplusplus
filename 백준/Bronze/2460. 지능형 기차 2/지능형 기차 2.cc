c979fdb4a84c76e28124cc4b4897bca2e09921e4
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-03 13:06:35 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, n = 0, m = 0;
+	for (int l = 0; l < 10; l++) {
+		cin >> a >> b;
+		n += (b - a);
+		if (n > 10000) { n = 10000; }
+		m = (n > m) ? n : m;
+	}
+	cout << m << endl;
+	return 0;
+}
