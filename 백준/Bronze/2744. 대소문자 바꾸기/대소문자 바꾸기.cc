3a9222dede2252e0b7af656e94a8dc541d143044
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-04 12:04:02 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a[101];
+	cin >> a;
+	char* b = a;
+	for (int n = 0; a[n] != NULL; n++) {
+		if ('a' <= *b && *b <= 'z') { *b = *b - 32; }
+		else { *b = *b + 32; }
+		b++;
+	}
+	cout << a << endl;
+	return 0;
+}
