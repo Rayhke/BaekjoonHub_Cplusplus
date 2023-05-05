5aa8e56fe7a6bde1af0e50c9ea05e1c4cd1a3857
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-06 06:14:30 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int h, m, s;
+	cin >> h >> m >> s;
+	if ((m + s) >= 60) {
+		h += (m + s) / 60;
+		m = (m + s) % 60;
+		if (h >= 24) { h = h % 24; }
+	}
+	else { m = m + s; }
+	cout << h << " " << m << endl;
+	return 0;
+}
