cad01699ce775d3fd5fdcf6b9834bde9b5093d90
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-06 02:07:05 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -2,16 +2,12 @@
 using namespace std;
 
 int main() {
-	int n, m;
-	double sum = 0, max = 0;
-	cin >> n;
-	for (int p = 0; p < n; p++) {
-		cin >> m;
-		sum += m;
-		if (m > max) {
-			max = m;
-		}
-	}
-	cout << sum / n / max * 100;
+	int a, b;
+	double s = 0, m = 0;
+	cin >> a;
+	for (int n = 0; n < a; n++) {
+		cin >> b; s += b;
+		m = (b > m) ? b : m;
+	} cout << (s / m / a) * 100;
 	return 0;
 }
