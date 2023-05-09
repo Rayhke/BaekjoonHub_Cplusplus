719fa4e7b4e17474dd56c21212cbfcac899f5a02
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-09 11:20:32 +0900

    Update 킹， 퀸， 룩， 비숍， 나이트， 폰 (2).cc

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰 (2).cc
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰 (2).cc
@@ -1,1 +1,12 @@
-asd
+#include <iostream>
+using namespace std;
+
+int main() {	/* 킹 1, 퀸 1, 룩 2, 비숍 2, 나이트 2, 폰 8 */
+	int King, Queen, Look, Bishop, Night, Phone;
+	cin >> King >> Queen >> Look >> Bishop >> Night >> Phone;
+	cout << 1 - King << " " << 1 - Queen << " " << 2 - Look << " " << 2 - Bishop << " " << 2 - Night << " " << 8 - Phone;
+	return 0;
+}
+/* ■ 설명 ■
+과거에 풀었던 방식
+*/
