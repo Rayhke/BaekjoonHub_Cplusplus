031c63918256df93cf254bacc07b39552391579c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-10 12:28:50 +0900

    Update 단어 공부.cc

--- 백준/Bronze/1157. 단어 공부/단어 공부.cc
+++ 백준/Bronze/1157. 단어 공부/단어 공부.cc
@@ -2,21 +2,23 @@
 using namespace std;
 
 int main() {
-	int a[26] = {}, b, c, m = 0, l;
+	int a[26] = {}, b, c, m = 0;
 	char d; string e;
 	cin >> e;
-	l = size(e);
-	for (int n = 0; n < l; n++) {
+	for (int n = 0; n < size(e); n++) {
 		b = e[n];
 		if (64 < b && b < 91) { a[b - 65] += 1; }
 		else if (96 < b && b < 123) { a[b - 97] += 1; }
 	}
-	for (int n = 0; n < 26; n++) { if (a[n] > m) { m = a[n]; c = n; } }
+	for (int n = 0; n < 26; n++) { if (a[n] > m) { m = a[n]; c = n; } }	// 최댓값을 찾는 동시에 최댓값 자릿값도 찾기
 	for (int n = 0; n < 26; n++) {
-		if (n == c) { continue; }
-		if (m == a[n]) { c = -1; break; }
+		if (n == c) { continue; }					// 최댓값이 담긴 자리는 검사 패스
+		if (m == a[n]) { c = -1; break; }				// 최댓값이 중복 된다면, (c = -1) 를 담은 후에 반복문 종료 
 	}
-	if (c != -1) { d = c + 65; cout << d; }
+	if (c != -1) { d = c + 65; cout << d; }					// 최댓값이 중복 되지 않는 다면, 알파벳 자릿값으로 정상 출력 될 것
 	else { cout << "?"; }
 	return 0;
 }
+/* ■ 설명 ■
+
+*/
