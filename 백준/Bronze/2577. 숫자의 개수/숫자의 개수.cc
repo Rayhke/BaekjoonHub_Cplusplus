23360527584dca4438be26f12ac1f8fc84b2a1e2
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-11 12:11:03 +0900

    Update 숫자의 개수.cc

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string>	// [to_string] 를 사용하기 위한 헤더 파일
 using namespace std;
 
 int main() {
@@ -7,7 +7,11 @@ int main() {
 	string e;
 	cin >> a >> b >> c;
 	e = to_string(a * b * c);
-	for (int n = 0; e[n] != '\0'; n++) { a = e[n]; d[a - 48] += 1; }
+	for (int n = 0; e[n] != '\0'; n++) { a = e[n] - '0'; d[a] += 1; }
 	for (int n = 0; n < 10; n++) { cout << d[n] << endl; }
 	return 0;
 }
+/* ■ 설명 ■
+
+
+*/
