534a5a829719ed67203d413d22c8344c1cf42e26
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-18 22:27:08 +0900

    Update 팩토리얼 2.cc

--- 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.cc
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
+long long s(long long n) {
+	if (n <= 1) { return 1; }
+	return (n * s(n - 1));
+}
+
 int main() {
-	long long a, b = 1;
-	cin >> a;
-	for (int n = 1; n <= a; n++) b = b * n;
-	if (b > 0) cout << b;
-	else cout << "1";
+	long long n;
+	cin >> n;
+	cout << s(n);
 	return 0;
 }
+/* ■ 설명 ■
+정석적인 방식인 재귀 함수를 이용하여 팩토리얼 기능을 구현한 간단한 예제
+*/
