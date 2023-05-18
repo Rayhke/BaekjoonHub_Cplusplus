f927908466e69b3e653d6a63bbff5008d7e3bf64
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-18 22:32:44 +0900

    Update 빠른 A＋B.cc

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -12,5 +12,6 @@ int main() {
 }
 /* ■ 설명 ■
 인라인 함수의 기능과
-[ios::sync_with_stdio(false);] [cin.tie(NULL);] 를 사용하여 빠른 연산을 시행함
+[ios::sync_with_stdio(false);] [cin.tie(NULL);] 를 사용하여
+무수하게 많은 연산 횟수도 빠르게 시행함
 */
