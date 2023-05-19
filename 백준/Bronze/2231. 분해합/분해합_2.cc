6de3172bb0328df08bbf07aa63e57896ba818ac6
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-19 13:56:57 +0900

    Update 분해합_2.cc

--- 백준/Bronze/2231. 분해합/분해합_2.cc
+++ 백준/Bronze/2231. 분해합/분해합_2.cc
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <string>		// [to_string] 를 사용하기 위한 헤더 파일 - < 설명 1 >
+#include <string>	// [to_string] 를 사용하기 위한 헤더 파일 - < 설명 1 >
 using namespace std;
 
 int s(int b) {
-	int c; string s = to_string(b);			  // [int] 형을 [string] 형 문자열로 변환
+	int c; string s = to_string(b);		// [int] 형을 [string] 형 문자열로 변환
 	for (int n = 0; s[n] != '\0'; n++) {
-		c = s[n] - 48;						          // [string] 에 담긴 숫자인 아스키 코드 값 이용
+		c = s[n] - 48;			// [string] 에 담긴 숫자인 아스키 코드 값 이용
 		if (c != 0) { b = b + c; }
 	}
 	return b;
@@ -14,10 +14,10 @@ int s(int b) {
 int main() {
 	int a, b = 0, c = 0;
 	cin >> a;
-	while (a > b) {                       // [while] 반복문에 종료 조건을 직접 넣어서, 처음 작성한 코드 보단
-    b = b + 1;                          // 간결해졌지만, 연산에 걸린 시간이 8 ms 증가 했다.
-    if (a == s(b)) { c = 1; break; }
-  }
+	while (a > b) {					// [while] 반복문에 종료 조건을 직접 넣어서, 처음 작성한 코드 보단
+		b = b + 1;                          	// 간결해졌지만, 연산에 걸린 시간이 8 ms 증가 했다. (총, 28 ms 소요)
+		if (a == s(b)) { c = 1; break; }
+  	}
 	if (c == 1) { cout << b; }
 	else { cout << c; }
 	return 0;
