8b760e3d36fc7419d9eef33e455af1d951327635
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-22 14:47:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[1000] = {}, b, c, N;
+	cin >> N;
+	for (int n = 0; n < N; n++) { cin >> a[n]; }
+	for (int n = 0; n < N; n++) {
+		b = n;
+		for (int m = n + 1; m < N; m++) {
+			if (a[b] > a[m]) { b = m; }
+		}
+		c = a[n];
+		a[n] = a[b];
+		a[b] = c;
+	}
+	for (int n = 0; n < N; n++) {
+		cout << a[n] << "\n";
+	}
+	return 0;
+}
