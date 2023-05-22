d93154d81c6aa1a9cc17dfb75ec3b6f9f43a6477
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-22 14:00:22 +0900

    [Bronze IV] Title: Petrol, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18330. Petrol/Petrol.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	if (n < 60 || (n <= (k + 60))) { cout << (n * 1500); }
+	else { cout << (((k + 60) * 1500) + ((n - (k + 60)) * 3000)); }
+	return 0;
+}
