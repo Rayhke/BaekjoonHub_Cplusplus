c7a1068b52f6bfda965a5ae77043e8c015d5205f
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-22 14:01:07 +0900

    Update Petrol.cc

--- 백준/Bronze/18330. Petrol/Petrol.cc
+++ 백준/Bronze/18330. Petrol/Petrol.cc
@@ -4,7 +4,11 @@ using namespace std;
 int main() {
 	int n, k;
 	cin >> n >> k;
-	if (n < 60 || (n <= (k + 60))) { cout << (n * 1500); }
-	else { cout << (((k + 60) * 1500) + ((n - (k + 60)) * 3000)); }
+	if (n < 60 || (n <= (k + 60))) {
+		cout << (n * 1500);
+	}
+	else {
+		cout << (((k + 60) * 1500) + ((n - (k + 60)) * 3000));
+	}
 	return 0;
 }
