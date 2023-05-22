e34d294213b109a4d923862a1fd9baf3446e887e
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-22 13:45:52 +0900

    Create 다이얼_2.cc

---
+++ 백준/Bronze/5622. 다이얼/다이얼_2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b = 0; string s;
+	cin >> s;
+	for (int n = 0; s[n] != '\0'; n++) {
+		a = s[n];
+		if ('A' <= s[n] && s[n] <= 'Z') {
+			if (64 < a && a < 68) { b += 3; }
+			else if (67 < a && a < 71) { b += 4; }
+			else if (70 < a && a < 74) { b += 5; }
+			else if (73 < a && a < 77) { b += 6; }
+			else if (76 < a && a < 80) { b += 7; }
+			else if (79 < a && a < 84) { b += 8; }
+			else if (83 < a && a < 87) { b += 9; }
+			else { b += 10; }
+		}
+	}
+	cout << b;
+	return 0;
+}
+/* ■ 설명 ■	 // 백준 - 브론즈 2 (5622번)
+다이얼 넘버로 알파벳을 입력 받았을 때,
+각 알파벳에 따라서
+
+A B C : 3초
+D E F : 4초
+G H I : 5초
+J K L : 6초
+M N O : 7초
+P Q R S : 8초
+T U V : 9초
+W X Y Z : 10초
+
+의 입력 시간이 걸린다.
+*/
