309f4ddb51760613bca04f2e4301f38c8f5e1e55
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-23 12:35:59 +0900

    Update 피보나치 수 5.cc

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -5,9 +5,14 @@ int main() {
 	int a[21] = {}, n, m = 0;
 	a[0] = 0; a[1] = 1; cin >> n;
 	if (n > 1) {
-		for (int l = 2; l <= n; l++) { a[l] = a[l - 1] + a[l - 2]; m = l; }
+		for (int l = 2; l <= n; l++) {
+			a[l] = a[l - 1] + a[l - 2]; m = l;
+		}
 	}
 	else { m = n; }
 	cout << a[m];
 	return 0;
 }
+/* ■ 설명 ■
+피보나치 수 공식을 이용한 배열 설계
+*/
