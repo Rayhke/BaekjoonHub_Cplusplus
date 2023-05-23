17feb6dfcf3d4302653ab1ae63cc8baf87ba5768
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-23 12:39:43 +0900

    Update 피보나치 수 5.cc

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -5,12 +5,10 @@ int main() {
 	int a[21] = {}, n, m = 0;
 	a[0] = 0; a[1] = 1; cin >> n;
 	if (n > 1) {
-		for (int l = 2; l <= n; l++) {
-			a[l] = a[l - 1] + a[l - 2]; m = l;
-		}
+		for (int l = 2; l <= n; l++) { a[l] = a[l - 1] + a[l - 2]; m = l; }
 	}
-	else { m = n; }
-	cout << a[m];
+	else { m = n; }		// 주의 해야할 점, 피보나치 수 1번 째 자리는 ' 1 '이며,
+	cout << a[m];		// 제출한 백준 문제에선 0번 째 자리에 나오는 값도 고려하는 듯 하다.
 	return 0;
 }
 /* ■ 설명 ■
