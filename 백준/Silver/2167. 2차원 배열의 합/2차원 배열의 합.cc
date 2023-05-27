d901c3987578e3f37329468ee6c6b915eb4018bd
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-05-27 13:23:30 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 420 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[300][300] = {}, b, c, d, e[10000] = {}, x[2], y[2];
+	cin >> b >> c;
+	for (int n = 0; n < b; n++) { for (int m = 0; m < c; m++) { cin >> a[n][m]; } }
+	cin >> d;
+	for (int n = 0; n < d; n++) {
+		cin >> x[0] >> y[0] >> x[1] >> y[1];
+		for (int m = x[0] - 1; m < x[1]; m++) { for (int l = y[0] - 1; l < y[1]; l++) { e[n] = e[n] + a[m][l]; } }
+	}
+	for (int n = 0; n < d; n++) { cout << e[n] << '\n'; }
+	return 0;
+}
