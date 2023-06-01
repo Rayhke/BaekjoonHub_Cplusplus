afd534670e7fc26acfb6c79b10abbaae83938d80
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-01 12:39:24 +0900

    Update 바구니 순서 바꾸기.cc

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.cc
@@ -9,11 +9,14 @@ int main() {
 	for (int n = 0; n < c; n++) {
 		cin >> d >> e >> f;
 		for (int m = d; m < f; m++) {
-			g = a[d - 1];
-			for (int l = d; l < e; l++) { a[l - 1] = a[l]; }
-			a[e - 1] = g;
+			g = a[d - 1];						// 앞자리 저장
+			for (int l = d; l < e; l++) { a[l - 1] = a[l]; }	// 배열 앞으로 잡아 당기기
+			a[e - 1] = g;						// 맨 뒷자리에 앞자리에 저장한 값 로드
 		}
 	}
 	for (int n = 0; n < b; n++) { cout << a[n] << " "; }
 	return 0;
 }
+/* ■ 설명 ■
+
+*/
