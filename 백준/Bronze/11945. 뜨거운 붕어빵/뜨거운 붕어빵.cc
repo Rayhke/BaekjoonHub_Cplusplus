f080a3196a9742008770a8be3d1cdd99cabf9e19
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-02 23:28:13 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	char c[1]; string s, p;
+	cin >> a >> b;
+	for (int n = 0; n < a; n++) {
+		cin >> s;
+		for (int m = 0; m < b / 2; m++) {
+			c[0] = s[m];
+			s[m] = s[b - m - 1];
+			s[b - m - 1] = c[0];
+		}
+		p += s; p += "\n";
+	}
+	cout << p;
+	return 0;
+}
