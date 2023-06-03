e0a276b73891063a9c3aaedbf18df8ea34453bb6
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-04 01:05:57 +0900

    [Bronze II] Title: 완전제곱수, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+inline int s(int a) { return pow(a, 2); }
+
+int main() {
+	int a, b, c, d = 0, M, N;
+	cin >> M >> N;
+	a = sqrt(M); b = pow(a, 2);
+	if (b < M) {
+		a = a + 1; b = pow(a, 2);
+		if (N < b) { cout << -1; return 0; }
+	}
+	do {
+		c = s(a); 
+		if (c > N) { break; }
+		else { d = d + c; }
+		a = a + 1;
+	} while (c <= N);
+	cout << d << '\n' << b;
+	return 0;
+}
