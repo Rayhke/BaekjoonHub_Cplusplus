97280d40ff0ff85ceff43c7b6751d5feecb17e43
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-04 13:38:39 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a[15][15] = {}, b, c, d, e[100000] = {};
+	cin >> b;
+	for (int n = 0; n < 15; n++) { a[0][n] = n + 1; a[n][0] = 1; }
+	for (int n = 1; n < 15; n++) {
+		for (int m = 1; m < 15; m++) {
+			a[n][m] = a[n][m - 1] + a[n - 1][m];
+		}
+	}
+	for (int n = 0; n < b; n++) {
+		cin >> c >> d;
+		e[n] = a[c][d - 1];
+	}
+	for (int n = 0; n < b; n++) { cout << e[n] << '\n'; }
+	return 0;
+}
