b8cc58d4622380477ef52e35544d9a73a1f260cf
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-06 23:21:50 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1732 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int* a = new int[10001]{}, b, c;
+	cin >> b;
+	for (int n = 0; n < b; n++) { cin >> c; a[c]++; }
+	for (int n = 1; n < 10001; n++) {
+		if (a[n] == 0) { continue; }
+		else {
+			b = a[n];
+			for (int m = 0; m < b; m++) {
+				cout << n << '\n';
+			}
+		}
+	}
+	return 0;
+}
