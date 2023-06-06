a617734fbafecc5c0ad67b7b295c180ec768c1ac
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-06 19:40:51 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a[10] = {}, b, c, d = -1; string s;
+	cin >> s;
+	for (int n = 0; s[n] != '\0'; n++) { b = s[n] - '0'; a[b]++; }
+	b = a[6] + a[9]; c = (b / 2) + (b % 2);
+	for (int n = 0; n < 10; n++) {
+		if (n == 6 || n == 9 || d >= a[n]) { continue; }
+		else if (d < a[n]) { d = a[n]; }
+	}
+	if (c < d) { cout << d; }
+	else { cout << c; }
+	return 0;
+}
