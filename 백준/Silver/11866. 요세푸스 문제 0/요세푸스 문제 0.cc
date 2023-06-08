3d91a5db7fb626e041626d6ee180f9012db49713
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-08 14:14:17 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K, a, b, c;
+	cin >> N >> K;
+	int* s = new int[N];
+	for (int n = 0; n < N; n++) { s[n] = n + 1; }
+	for (int n = 0; n < N; n++) {
+		for (int m = 0; m < K - 1; m++) {
+			a = s[n];
+			for (int l = n; l < N - 1; l++) { s[l] = s[l + 1]; }
+			s[N - 1] = a;
+		}
+	}
+	cout << "<";
+	for (int n = 0; n < N; n++) { cout << s[n]; if ((N - 1) - n) { cout << ", "; } }
+	cout << ">";
+	return 0;
+}
