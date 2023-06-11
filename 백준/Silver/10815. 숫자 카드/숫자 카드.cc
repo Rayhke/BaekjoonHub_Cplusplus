61a4bc39067fec6b7764f8d4d478e6de4f47b87a
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-11 17:51:06 +0900

    [Silver V] Title: 숫자 카드, Time: 236 ms, Memory: 5888 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int com(const void* a, const void* b) {
+    int elt1 = *static_cast<const int*>(a);
+    int elt2 = *static_cast<const int*>(b);
+    if (elt1 < elt2) { return -1; }
+    else if (elt1 > elt2) { return 1; }
+    else { return 0; }
+}
+
+int s(int a, int b[], int e) {
+    int mn = 0, md, mx = a - 1;
+    while (mn <= mx) {
+        md = (mn + mx) / 2;
+        if (b[md] == e) { return 1; }
+        else if (b[md] < e) { mn = md + 1; }
+        else { mx = md - 1; }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, c, e; cin >> a;
+	int* b = new int[a];
+	for (int n = 0; n < a; n++) { cin >> b[n]; }
+    qsort(b, a, sizeof(int), com);
+    cin >> c; int* d = new int[c];
+    for (int n = 0; n < c; n++) {
+        cin >> e;
+        d[n] = s(a, b, e);
+    }
+    for (int n = 0; n < c; n++) { cout << d[n] << " "; }
+    delete[]b, d;
+	return 0;
+}
