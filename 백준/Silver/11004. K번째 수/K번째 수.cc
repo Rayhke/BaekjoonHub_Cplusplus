f263a19d411028eec9aec34805f960ff2c9ffe07
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-11 18:32:04 +0900

    [Silver V] Title: K번째 수, Time: 1040 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, K, o, p; cin >> N >> K;
+	int* a = new int[N] {};
+	for (int n = 0; n < N; n++) { cin >> a[n]; }
+	sort(a, a + N);
+	cout << a[K - 1];
+	return 0;
+}
