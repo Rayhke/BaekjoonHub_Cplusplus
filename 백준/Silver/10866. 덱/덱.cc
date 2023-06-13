e69602551d17e38dd2243d2bd0051e8d6797c1a9
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-13 14:45:30 +0900

    [Silver IV] Title: 덱, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+deque <int> dq;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b; cin >> a; string s;
+	for (int n = 0; n < a; n++) {
+		cin >> s;
+		if (s == "push_front") { cin >> b; dq.push_front(b); }
+		else if (s == "push_back") { cin >> b; dq.push_back(b); }
+		else if (s == "pop_front") { if (dq.empty() != 1) { cout << dq.front() << '\n'; dq.pop_front(); } else { cout << -1 << '\n'; } }
+		else if (s == "pop_back") { if (dq.empty() != 1) { cout << dq.back() << '\n'; dq.pop_back(); } else { cout << -1 << '\n'; } }
+		else if (s == "size") { cout << dq.size() << '\n'; }
+		else if (s == "empty") { cout << dq.empty() << '\n'; }
+		else if (s == "front") { if (dq.empty() != 1) { cout << dq.front() << '\n'; } else { cout << -1 << '\n'; } }
+		else if (s == "back") { if (dq.empty() != 1) { cout << dq.back() << '\n'; } else { cout << -1 << '\n'; } }
+		s = "";
+	}
+	return 0;
+}
