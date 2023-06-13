c25208ac0c87d92ac55f206ca808266155b7e113
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-13 18:56:34 +0900

    Update 회전하는 큐.cc

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -10,18 +10,18 @@ int main() {
 	for (int n = 0; n < N; n++) { dq.push_back(n + 1); }
 	for (int n = 0; n < M; n++) {
 		cin >> a; b = dq.size();
-		for (int m = 0; m < b; m++) {
+		for (int m = 0; m < b; m++) {		// 앞으로 돌린 경우의 수
 			if (dq.front() == a) { c = m; }
 			dq.push_back(dq.front());
 			dq.pop_front();
 		}
-		for (int m = 0; m < b; m++) {
+		for (int m = 0; m < b; m++) {		// 뒤로 돌린 경우의 수
 			if (dq.front() == a) { d = m; }
 			dq.push_front(dq.back());
 			dq.pop_back();
 		}
-		if (c == 0 && d == 0) { dq.pop_front(); }
-		else if (c < d) {
+		if (c == 0 && d == 0) { dq.pop_front(); }	// 만약, [c] 와 [d] 의 값이 0 인 경우의 에러 방지
+		else if (c < d) {				// 추후에 구체적인 원인 분석 바람
 			for (int m = 0; m < c; m++) {
 				dq.push_back(dq.front());
 				dq.pop_front();
