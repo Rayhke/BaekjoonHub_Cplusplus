0e885744237c634321b4fb5d2232a60b422247f9
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-18 13:51:14 +0900

    [Silver IV] Title: 로프, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+inline int s(int a, int p[], int n) { return (p[n] * (a - n)); }
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b = 0; cin >> a;
+	int* p = new int[a] {};
+	for (int n = 0; n < a; n++) { cin >> p[n]; }
+	sort(p, p + a);
+	for (int n = 0; n < a; n++) {
+		if (b < s(a, p, n)) { b = s(a, p, n); }
+	}
+	cout << b;
+	delete[]p;
+	return 0;
+}
