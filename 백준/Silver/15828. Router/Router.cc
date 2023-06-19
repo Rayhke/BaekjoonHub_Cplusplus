7d83dbf25c520f64d047d8b51e7547978813b720
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-19 18:27:15 +0900

    Update Router.cc

--- 백준/Silver/15828. Router/Router.cc
+++ 백준/Silver/15828. Router/Router.cc
@@ -7,11 +7,11 @@ queue <int> q;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int a, b = 0; cin >> a;
-	while (b != -1) {
+	do {
 		cin >> b;
-		if (b > 0 && (q.size() < a)) { q.push(b); }
+		if (b > 0 && (q.size() < a)) { q.push(b); }	// 'q.size() < a' 이게 핵심이다.
 		else if (b == 0 && !q.empty()) { q.pop(); }
-	}
+	} while (b != -1);
 	if (!q.empty()) { while (!q.empty()) { cout << q.front() << " "; q.pop(); } }
 	else { cout << "empty"; }
 	return 0;
