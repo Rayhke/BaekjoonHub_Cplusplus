2aaad4191c02d197f7f520a7e1cbec140e044794
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-20 06:45:57 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int C, T; cin >> T;
+	int** p = new int* [T] {};
+	for (int n = 0; n < T; n++) { p[n] = new int[4]{}; }
+	for (int n = 0; n < T; n++) {
+		cin >> C;
+		while (1) {
+			if (C >= 25) { p[n][0] = C / 25; C = C % 25; }
+			else if (C >= 10) { p[n][1] = C / 10; C = C % 10; }
+			else if (C >= 5) { p[n][2] = C / 5; C = C % 5; }
+			else { p[n][3] = C; break; }
+		}
+	}
+	for (int n = 0; n < T; n++) { cout << p[n][0] << " " << p[n][1] << " " << p[n][2] << " " << p[n][3] << '\n'; }
+	for (int n = 0; n < T; n++) { delete[]p[n]; }
+	delete[]p;
+	return 0;
+}
