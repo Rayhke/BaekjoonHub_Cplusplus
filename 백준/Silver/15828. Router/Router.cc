a99b87ad0895af76e84720e8e0e645a39c88d068
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-19 18:27:39 +0900

    Update Router.cc

--- 백준/Silver/15828. Router/Router.cc
+++ 백준/Silver/15828. Router/Router.cc
@@ -9,7 +9,7 @@ int main() {
 	int a, b = 0; cin >> a;
 	do {
 		cin >> b;
-		if (b > 0 && (q.size() < a)) { q.push(b); }	// 'q.size() < a' 이게 핵심이다.
+		if (b > 0 && (q.size() < a)) { q.push(b); }	// 'q.size() < a' 이게 핵심이다. 배열의 크기를 지정하듯, 흉내내기 가능함
 		else if (b == 0 && !q.empty()) { q.pop(); }
 	} while (b != -1);
 	if (!q.empty()) { while (!q.empty()) { cout << q.front() << " "; q.pop(); } }
