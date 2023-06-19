55ac10db732a217da3e0af0d580b9fc1fce480ab
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-20 07:28:45 +0900

    Create 한조서열정리하고옴ㅋㅋ_2.cc

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ_2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+inline int s(int n, int m, int p[]) {
+	if (p[n] > p[m]) { return 1; }
+	else { return 0; }
+}
+
+int main() {
+	int a, b = 0, c = 0; cin >> a;
+	int* p = new int [a] {};
+	for (int n = 0; n < a; n++) { cin >> p[n]; }
+	for (int n = 0; n < a; n++) {
+		for (int m = n + 1; m < a; m++) {
+			if (s(n, m, p)) { c = c + 1; }
+			else { break; }
+		}
+		if (b < c) { b = c; }c = 0;
+	}
+	cout << b;
+	delete[]p;
+	return 0;
+}
