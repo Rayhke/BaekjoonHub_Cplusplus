e3854e94f5262d8ec6bab2287751ac3eebecbd13
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-21 12:23:43 +0900

    Update 슈퍼 마리오.cc

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.cc
@@ -8,7 +8,7 @@ int main() {
 		if (a[n] + b < 100) { b += a[n]; }
 		else { c = a[n] + b; break; }
 	}
-	if ((100 - b < c - 100) || c == 0) { cout << b; }
+	if ((100 - b < c - 100) || c == 0) { cout << b; }	// 왜 굳이 c == 0 을 추가 했는 가? 그것이 핵심이다. 잘 분석해봐라
 	else if (100 - b >= c - 100) { cout << c; }
 	return 0;
 }
