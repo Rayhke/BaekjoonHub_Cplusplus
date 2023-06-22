82a2aaa0c2b13f5d564980a54b114eabaad4cd14
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-22 20:54:07 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a[100] = {}, b = 0, N; cin >> N;
+	for (int n = 0; n < N; n++) { cin >> a[n]; }
+	for (int n = N - 1; n > 0; n--) {
+		if (a[n - 1] >= a[n]) {
+			if (a[n] - 1 > 0) {
+				b += a[n - 1] - a[n] + 1;
+				a[n - 1] = a[n] - 1;
+			}
+		}
+	}
+	cout << b;
+	return 0;
+}
