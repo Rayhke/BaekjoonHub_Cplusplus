fbf4d67d39c23ea777582a3e99b3f6d546f77455
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-23 09:28:28 +0900

    Update 직각 삼각형의 두 변.cc

--- 백준/Bronze/6322. 직각 삼각형의 두 변/직각 삼각형의 두 변.cc
+++ 백준/Bronze/6322. 직각 삼각형의 두 변/직각 삼각형의 두 변.cc
@@ -9,7 +9,6 @@ inline float s(int a, int b, int c) {
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int a, b, c, d = 1; float e;
 	do {
 		cout << fixed;
@@ -32,3 +31,14 @@ int main() {
 	} while (a + b + c != 0);
 	return 0;
 }
+/* ■ 설명 ■
+[a] 높이, [b] 밑변, [c] 빗변이며
+세 가지 중에 반드시 하나는 ' -1 ' 을 입력 받게 되는 데
+이 경우는 모르는 값이기에 찾아야 되는 값이다.		// (a = ?) or (b = ?) or (c = ?)
+그렇다면 피타고라스 정리를 이용하여 풀으면 간단하되
+만약 성립하지 않는 다면, "Impossible." 를 출력한다.
+=====================================================
+[오답 원인]
+
+백준 기준으로 출력 후 뒤에 "\n\n" 인 경우도 있다.
+*/
