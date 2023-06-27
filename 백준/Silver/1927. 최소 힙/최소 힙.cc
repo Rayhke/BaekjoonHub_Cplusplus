6b56169720eb1d0e0ff18adf7cede668fc43f61f
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-27 14:26:02 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue <int, vector<int>, greater<int>> pq;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, N; cin >> N;
+	for (int n = 0; n < N; n++) {
+		cin >> a;
+		if (a > 0) { pq.push(a); }
+		else if (a == 0) {
+			if (!pq.empty()) { cout << pq.top() << '\n'; pq.pop(); }
+			else { cout << 0 << '\n'; }
+		}
+	}
+	return 0;
+}
