6fecc860153581d3d4fc8a07c42261b18c8afb5c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-29 18:26:14 +0900

    [Silver V] Title: 나이순 정렬, Time: 72 ms, Memory: 7904 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+bool p(pair<int, string> v1, pair<int, string>v2) { return v1.first < v2.first; }
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a; cin >> a;
+	vector <pair<int, string>> v(a);
+	for (int n = 0; n < a; n++) { cin >> v[n].first >> v[n].second; }
+	stable_sort(v.begin(), v.end(), p);
+	for (int n = 0; n < a; n++) { cout << v[n].first << ' ' << v[n].second << '\n'; }
+	return 0;
+}
