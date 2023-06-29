b14d1759857ec3d700e3f5f0cdc5ab21cf5c6545
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-29 18:26:50 +0900

    Update 나이순 정렬.cc

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -9,9 +9,11 @@ bool p(pair<int, string> v1, pair<int, string>v2) { return v1.first < v2.first; 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int a; cin >> a;
-	vector <pair<int, string>> v(a);
+	vector <pair<int, string>> v(a);					// 굳이 이렇게 한 이유는
 	for (int n = 0; n < a; n++) { cin >> v[n].first >> v[n].second; }
 	stable_sort(v.begin(), v.end(), p);
 	for (int n = 0; n < a; n++) { cout << v[n].first << ' ' << v[n].second << '\n'; }
 	return 0;
 }
+/*
+*/
