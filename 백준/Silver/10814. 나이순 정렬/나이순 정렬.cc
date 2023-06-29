76f3c30e944f60e7c47c60d0b418eb8d0d46f29f
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-06-29 18:15:49 +0900

    [Silver V] Title: 나이순 정렬, Time: 112 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+vector <tuple<int, int, string>> v;
+
+bool p(tuple<int, int, string> v1, tuple<int, int, string> v2) {
+	if (get<0>(v1) != get<0>(v2)) { return get<0>(v1) < get<0>(v2); }
+	else { return get<1>(v1) < get<1>(v2); }
+}
+
+int main() {
+	int a, b; string s; cin >> a;
+	for (int n = 0; n < a; n++) {
+		cin >> b >> s;
+		v.push_back(make_tuple(b, n, s));
+	}
+	sort(v.begin(), v.end(), p);
+	for (int n = 0; n < a; n++) {
+		cout << get<0>(v[n]) << ' ' << get<2>(v[n]) << '\n';
+	}
+	return 0;
+}
