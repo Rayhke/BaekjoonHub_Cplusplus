f19f404ce8fc06321e00b8326e86c79cf3c3cd05
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-03 18:32:15 +0900

    [Silver III] Title: 합 구하기, Time: 40 ms, Memory: 3056 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector <int> v;
+queue <int> q;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int a, b, c = 0; cin >> a;
+	for (int n = 0; n < a; n++) {
+		if (n > 0) { c = v[n - 1]; }
+		cin >> b; v.push_back(b + c);
+	}
+	cin >> a;
+	for (int n = 0; n < a; n++) {
+		cin >> b >> c;
+		if (b == 1) { q.push(v[c - 1]); }
+		else { q.push(v[c - 1] - v[b - 2]); }
+	}
+	while (!q.empty()) {
+		cout << q.front() << '\n'; q.pop();
+	}
+	return 0;
+}
