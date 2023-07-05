10e195c3334387c6a587fc2c6084f60dca9876bd
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-05 12:58:44 +0900

    Update 숫자 카드 2.cc

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm>	// upper_bound, lower_bound
 using namespace std;
 
 vector <int> v;
@@ -16,3 +16,11 @@ int main() {
 	}
 	return 0;
 }
+/* ■ 설명 ■
+algorithm 헤더파일에 존재 하는
+upper_bound, lower_bound 의 기능은 무엇이느냐?
+upper_bound 는 배열의 시작 지점과 끝 지점을 지정 후, 특정 원소 값 보다 '이하' 인 원소의 갯수를 출력하고,
+lower_bound 는 배열의 시작 지점과 끝 지점을 지정 후, 특정 원소 값 보다 '이상' 인 원소의 갯수를 출력한다.
+이 점을 합쳐서 응용하면
+1 이상 또는 이하 만 찾게 된다 (즉, 1 이외의 원소 값은 출력하지 않음!)
+*/
