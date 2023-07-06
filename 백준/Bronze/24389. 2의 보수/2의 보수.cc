54be37f45ee5ec31e1f7e49ba98fd43d76920f6c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-06 11:25:07 +0900

    Update 2의 보수.cc

--- 백준/Bronze/24389. 2의 보수/2의 보수.cc
+++ 백준/Bronze/24389. 2의 보수/2의 보수.cc
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
     int a, b, c = 0; string s = "", p = "";
-    cin >> a; b = ~a + 1;
+    cin >> a; b = ~a + 1;               // 2의 보수는 모든 비트를 반전 후, + 1
     for (int n = 31; n > -1; n--) {
-        if (a & (1 << n)) { s += '1'; }
-        else { s += '0'; }
-        if (b & (1 << n)) { p += '1'; }
-        else { p += '0'; }
+        if (a & (1 << n)) { s += '1'; } // n번 째 위치로 비트 값 ' 1 ' 을 이동 후,
+        else { s += '0'; }              // ' AND ' 연산을 한 것.
+        if (b & (1 << n)) { p += '1'; } // n번 째 위치로 비트 값 ' 1 ' 을 이동 후,
+        else { p += '0'; }              // ' AND ' 연산을 한 것.
     }
     for (int n = 0; n < 32; n++) {
         if (s[n] != p[n]) { c++; }
