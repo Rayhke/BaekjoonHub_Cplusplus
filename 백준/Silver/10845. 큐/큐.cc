33b71c63f7ba28449d886c241f3d876c489438e6
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-07 14:19:19 +0900

    Update 큐.cc

--- 백준/Silver/10845. 큐/큐.cc
+++ 백준/Silver/10845. 큐/큐.cc
@@ -1,45 +1,20 @@
 #include <iostream>
+#include <queue>
 using namespace std;
+
+queue <int> q;
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	int a, b, c = -1, d[10000] = {}, e = 0;
-	string s;
-	cin >> a;
+	int a, b; cin >> a; string s;
 	for (int n = 0; n < a; n++) {
 		cin >> s;
-		if (s == "push") {
-			cin >> b;
-			d[++c] = b;
-		}
-		else if (s == "pop") {
-			if (d[0] != 0) {
-				cout << d[0] << '\n'; d[0] = 0;
-				if (c > -1) { c = c - 1; }
-				e = 0;
-				while (e <= c) {
-					d[e] = d[e + 1];
-					e = e + 1;
-				}
-			}
-			else { cout << -1 << '\n'; }
-		}
-		else if (s == "size") {
-			if (d[0] != 0) { cout << c + 1 << '\n'; }
-			else { cout << 0 << '\n'; }
-		}
-		else if (s == "empty") {
-			if (d[0] == 0) { cout << 1 << '\n'; }
-			else { cout << 0 << '\n'; }
-		}
-		else if (s == "front") {
-			if (d[0] != 0) { cout << d[0] << '\n'; }
-			else { cout << -1 << '\n'; }
-		}
-		else if (s == "back") {
-			if (c > -1 && d[c] != 0) { cout << d[c] << '\n'; }
-			else { cout << -1 << '\n'; }
-		}
+		if (s == "push") { cin >> b; q.push(b); }
+		else if (s == "pop") { if (q.empty() != 1) { cout << q.front() << '\n'; q.pop(); } else { cout << -1 << '\n'; } }
+		else if (s == "size") { cout << q.size() << '\n'; }
+		else if (s == "empty") { cout << q.empty() << '\n'; }
+		else if (s == "front") { if (q.empty() != 1) { cout << q.front() << '\n'; } else { cout << -1 << '\n'; } }
+		else if (s == "back") { if (q.empty() != 1) { cout << q.back() << '\n'; } else { cout << -1 << '\n'; } }
 		s = "";
 	}
 	return 0;
