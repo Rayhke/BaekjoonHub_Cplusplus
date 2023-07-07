1f344e5a23754a9e2e34e2b0bf39735233e68ac8
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-07 18:51:11 +0900

    Update 단어순서 뒤집기.cc

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.cc
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.cc
@@ -6,13 +6,13 @@ using namespace std;
 stack <string> s;
 
 int main() {
-	int N; string L, P = ""; cin >> N; cin.ignore();
+	int N; string L, P = ""; cin >> N; cin.ignore();	// cin >> N; 입력 때의 버퍼 지우기
 	for (int n = 1; n <= N; n++) {
 		getline(cin, L);
 		for (int m = 0; m < size(L); m++) {
 			if (L[m] == ' ') { continue; }
 			P += L[m];
-			if (L[m + 1] == ' ' || m + 1 == size(L)) { P += " "; s.push(P); P = ""; }
+			if (L[m + 1] == ' ' || m + 1 == size(L)) { P += " "; s.push(P); P = ""; }	// 마지막 끝에 도달 하였을 때, m + 1 == size(L)
 		}
 		cout << "Case #" << n << ": ";
 		while (!s.empty()) {
@@ -22,3 +22,7 @@ int main() {
 	}
 	return 0;
 }
+/* ■ 설명 ■
+cin 입력을 했을 시, 끝자리에 '\n'이 입력 되는 데, 그것을 무조건 지워 줘야
+getline 을 할 때, 입력이 밀리는 것을 방지한다.
+*/
