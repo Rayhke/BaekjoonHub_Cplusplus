df72798a3be0c23380f1f9b1ca931ea40b5c3021
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-07 19:19:21 +0900

    Update Cutting Corners.cc

--- 백준/Bronze/20215. Cutting Corners/Cutting Corners.cc
+++ 백준/Bronze/20215. Cutting Corners/Cutting Corners.cc
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <cmath>	// pow, sqrt
+#include <iomanip>	// setprecision
 using namespace std;
 
 int main() {
@@ -10,3 +10,11 @@ int main() {
 	cout << a + b - sqrt(pow(a, 2) + pow(b, 2));
 	return 0;
 }
+/* ■ 설명 ■
+끝 모서리 부분에 얼룩이 묻어서
+얼룩 묻은 부분만 잘라내고자 할 때
+
+이것을 직사각형으로 자르는 길이[a + b]와
+대각선 (피타고라스 정리) 길이[√(a²+ b²)]로 자르는 길이의
+차이를 출력하라.
+*/
