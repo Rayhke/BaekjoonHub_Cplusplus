abf2242ad658942b7eee3218a70c02db54b07dbd
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-09 16:13:32 +0900

    [Bronze IV] Title: 방학 숙제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5532. 방학 숙제/방학 숙제.cc
+++ 백준/Bronze/5532. 방학 숙제/방학 숙제.cc
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
 	int L, A, B, C, D;
 	cin >> L >> A >> B >> C >> D;
-	for (int n = 0; n < L; n++) {
+	for (int n = 1; n <= L; n++) {
 		A -= C; B -= D;
-		if (A <= 0 && B <= 0) { L -= n + 1; break; }
+		if (A <= 0 && B <= 0) { L -= n; break; }
 	}
 	cout << L;
 	return 0;
