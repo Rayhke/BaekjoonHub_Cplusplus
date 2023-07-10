103468de25b2df9559c8770fe16b966584393504
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-10 17:43:50 +0900

    Create 팰린드롬 만들기_2.cc

---
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기_2.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack <char> s;
+
+int main() {
+	bool r = 1; char c, d; int R[26] = {}, P = 0; string p;
+	cin >> p;
+	for (int n = 0; p[n] != '\0'; n++) { R[p[n] - 65]++; }
+	if (size(p) & 1) {
+		for (int n = 0; n < 26; n++) {
+			if (R[n] & 1) { P++; d = n + 65; }
+			if (P > 1) { r = 0; break; }
+		}
+	}
+	else {
+		for (int n = 0; n < 26; n++) {
+			if (R[n] & 1) { r = 0; break; }
+		}
+	}
+	if (r) {
+		for (int n = 0; n < 26; n++) {
+			if (R[n] != 0) {
+				for (int m = 0; m < R[n] / 2; m++) {
+					c = n + 65; cout << c;
+					s.push(c);
+				}
+			}
+		}
+		if (P & 1) { cout << d; }
+		while (!s.empty()) { cout << s.top(); s.pop(); }
+	}
+	else { cout << "I'm Sorry Hansoo"; }
+	return 0;
+}
