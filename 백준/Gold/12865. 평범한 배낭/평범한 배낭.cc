09891f8a135a0acc58d363ede01f5ca0d5cbba06
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-12 16:40:18 +0900

    [Gold V] Title: 평범한 배낭, Time: 40 ms, Memory: 42004 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -3,9 +3,9 @@
 #include <algorithm>
 using namespace std;
 
-int s(int N, int K, vector <int> w, vector <int> v) {	// 물건 총 갯수, 최대 무게 한도, 각 물건 무게, 각 물건 값 어치
+int s(int N, int K, vector <int> w, vector <int> v) {
 	int Ws, Vs;
-	vector<vector<int>> DP(N + 1, vector<int>(K + 1, 0));	// 2차원 배열로 선언 [열 : N + 1], [행 : K + 1] 원소 ' 0 ' 으로 초기화
+	vector<vector<int>> DP(N + 1, vector<int>(K + 1, 0));
 	for (int n = 1; n <= N; n++) {
 		Ws = w[n - 1]; Vs = v[n - 1];
 		for (int m = 1; m <= K; m++) {
@@ -17,9 +17,9 @@ int s(int N, int K, vector <int> w, vector <int> v) {	// 물건 총 갯수, 최�
 }
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL);
+    ios::sync_with_stdio(false); cin.tie(NULL);
 	int N, K, W, V; cin >> N >> K;
-	vector<int> w(N), v(N);				// 무게, 가격을 넣는 배열
+	vector<int> w(N), v(N);
 	for (int n = 0; n < N; n++) {
 		cin >> W >> V;
 		w[n] = W; v[n] = V;
