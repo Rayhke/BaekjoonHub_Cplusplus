b590ec24bb656aa3e6b3f324709d66a01661185c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-17 14:21:55 +0900

    [Silver III] Title: 분수 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <numeric>
+using namespace std;
+
+int main() {
+	int a[2], b[2], c, d; cin >> a[0] >> a[1] >> b[0] >> b[1];
+	c = lcm(a[1], b[1]);
+	a[0] *= c / a[1]; b[0] *= c / b[1];
+	d = gcd(a[0] + b[0], c);
+	if (d != 1) { cout << (a[0] + b[0]) / d << ' ' << c / d; }
+	else { cout << a[0] + b[0] << ' ' << c; }
+	return 0;
+}
