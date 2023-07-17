703f19b0b2616b59e05dfa5b7ac9b816633a650b
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-17 16:02:40 +0900

    [Bronze I] Title: 한조서열정리하고옴ㅋㅋ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14659. 한조서열정리하고옴ㅋㅋ/한조서열정리하고옴ㅋㅋ.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int v[30000], N, H = 0, M = 0, V = 0; cin >> N;
+	for (int n = 0; n < N; n++) { cin >> v[n]; }
+	for (int n = 0; n < N; n++) {
+		if (V) { V--; continue; }
+		for (int m = n + 1; m < N; m++) {
+			if (v[n] > v[m]) { H++; V++; }
+			else { break; }
+		}
+		M = (H > M) ? H : M; H = 0;
+	}
+	cout << M;
+	return 0;
+}
