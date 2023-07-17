6a3d4d5fc0ca1465db8ebdafe69458965ee398b0
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-17 15:26:53 +0900

    Update 보물.cc

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -5,7 +5,6 @@ using namespace std;
 inline int p(int a[], int b[], int n) { return a[n] * b[n]; }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int a[50] = {}, b[50] = {}, N, S = 0;
 	cin >> N;
 	for (int n = 0; n < N; n++) { cin >> a[n]; }
