b1631269a25fd172dd279b2c9d02a6814cd94a5c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-20 23:34:13 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	char C[101][101]; int N, P = 0, X = 0, Y = 0; cin >> N;
+	for (int n = 0; n < N; n++) {
+		for (int m = 0; m < N; m++) {
+			cin >> C[n][m];
+		}
+	}
+	for (int n = 0; n < N; n++) {
+		P = 0;
+		for (int m = 0; m < N; m++) {
+			if (C[n][m] == '.') { P++; if (P == 2) { X++; } }
+			else { P = 0; }
+		}
+	}
+	for (int n = 0; n < N; n++) {
+		P = 0;
+		for (int m = 0; m < N; m++) {
+			if (C[m][n] == '.') { P++; if (P == 2) { Y++; } }
+			else { P = 0; }
+		}
+	}
+	cout << X << ' ' << Y;
+	return 0;
+}
