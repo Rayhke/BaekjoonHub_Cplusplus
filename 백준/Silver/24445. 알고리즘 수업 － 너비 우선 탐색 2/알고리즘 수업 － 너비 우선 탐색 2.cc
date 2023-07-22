b7325f40c64344ff9d6fba2d19d3b50c0a5132b4
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-22 21:52:53 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 88 ms, Memory: 8936 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector <int> v[100001];
+int w[100001] = {};
+bool r[100001] = {};
+
+void B(int R) {
+	int S = 1;
+	queue <int> q; q.push(R);
+	r[R] = 1; w[R] = S;
+	while (!q.empty()) {
+		int X = q.front(); q.pop();
+		for (int n = v[X].size() - 1; n > -1; n--) {
+			int Y = v[X][n];
+			if (!r[Y]) {
+				S++;
+				r[Y] = 1; w[Y] = S;
+				q.push(Y);
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int N, M, R, x, y; cin >> N >> M >> R;
+	for (int n = 0; n < M; n++) {
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	for (int n = 1; n <= N; n++) { sort(v[n].begin(), v[n].end()); }
+	B(R);
+	for (int n = 1; n <= N; n++) { cout << w[n] << '\n'; }
+	return 0;
+}
