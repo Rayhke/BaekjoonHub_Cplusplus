82ed59f17f88584f2732ef79c5381093c0d447bc
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-25 12:51:50 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 96 ms, Memory: 11024 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector <int> v[100001];
+int w[100001];
+bool r[100001];
+int S = 1;
+
+void D(int R) {
+	r[R] = 1; w[R] = S;
+	for (int n = v[R].size() - 1; n > -1; n--) {
+		if (!r[v[R][n]]) { S++; D(v[R][n]); }
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	int N, M, R, x, y; cin >> N >> M >> R;
+	for (int n = 0; n < M; n++) {
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	for (int n = 1; n <= N; n++) { sort(v[n].begin(), v[n].end()); }
+	D(R);
+	for (int n = 1; n <= N; n++) { cout << w[n] << '\n'; }
+	return 0;
+}
