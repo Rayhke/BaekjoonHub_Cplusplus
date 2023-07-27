2d0cc7cd92b454444ee0869fbcb3aeee5493de01
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-27 21:38:10 +0900

    [Silver I] Title: 안전 영역, Time: 32 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+short v[100][100];
+bool r[100][100], q = 0;
+
+void V(int n, int m, int N, int H) {
+	stack <int> x, y; x.push(m); y.push(n);
+	while (!x.empty() && !y.empty()) {
+		short X = x.top(), Y = y.top(); x.pop(); y.pop();
+		if (r[Y][X] == q && v[Y][X] > H) {
+			r[Y][X] = !q; short XU = X + 1, XD = X - 1, YU = Y + 1, YD = Y - 1;
+			if (XU < N) { if (r[Y][XU] == q) { x.push(XU); y.push(Y); } }
+			if (XD > -1) { if (r[Y][XD] == q) { x.push(XD); y.push(Y); } }
+			if (YU < N) { if (r[YU][X] == q) { x.push(X); y.push(YU); } }
+			if (YD > -1) { if (r[YD][X] == q) { x.push(X); y.push(YD); } }
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	short O = 0, P = 0, Q = 0; int N; cin >> N;
+	for (int n = 0; n < N; n++) {
+		for (int m = 0; m < N; m++) {
+			cin >> v[n][m];
+			O = (v[n][m] > O) ? v[n][m] : O;
+		}
+	}
+	for (int H = 0; H < O; H++) {
+		Q = 0;
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < N; m++) {
+				if (r[n][m] == q && v[n][m] > H) {
+					Q++; V(n, m, N, H);
+				}
+			}
+		}
+		P = (Q > P) ? Q : P; q = !q;
+	}
+	cout << P;
+	return 0;
+}
