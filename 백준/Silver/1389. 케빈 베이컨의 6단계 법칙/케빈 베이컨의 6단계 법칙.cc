a3d9b016ffb38ab38d8de85687f51cc9a52955ad
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-29 03:44:54 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector <int> v[101];
+int w[101][101], Sum[101];
+bool r[101], R = 0;
+
+void B(int m,int N) {
+	queue <int> q; q.push(m); w[m][m] = 0; r[m] = !R;
+	while (!q.empty()) {
+		int X = q.front(); q.pop();
+		for (int n = 0; n < v[X].size(); n++) {
+			int Y = v[X][n];
+			if (r[Y] == R) { r[Y] = !R; w[m][Y] = w[m][X] + 1; q.push(Y); }
+		}
+	}
+	for (int n = 1; n <= N; n++) { Sum[m] += w[m][n]; }
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	int N, M, P = 1000000000, x, y; cin >> N >> M;
+	for (int n = 0; n < M; n++) {
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	for (int n = 1; n <= N; n++) { sort(v[n].begin(), v[n].end()); }
+	for (int n = 1; n <= N; n++) { B(n, N); R = !R; }
+	for (int n = 1; n <= N; n++) {
+		if (P > Sum[n]) { P = Sum[n]; x = n; }
+	}
+	cout << x;
+	return 0;
+}
