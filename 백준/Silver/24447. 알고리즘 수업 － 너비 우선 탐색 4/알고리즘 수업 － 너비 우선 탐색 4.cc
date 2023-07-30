d199d168a258a0e753000b465f781c0b7c4f2559
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-07-30 17:27:48 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 88 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+vector <int> v[100001];
+int w[100001], W[100001], S = 1;
+bool r[100001];
+
+void B(int R) {
+	queue <int> q; q.push(R); w[R] = S; W[R] = 0; r[R] = 1;
+	while (!q.empty()) {
+		int X = q.front(); q.pop();
+		for (int n = 0; n < v[X].size(); n++) {
+			int Y = v[X][n];
+			if (!r[Y]) { S++; r[Y] = 1; w[Y] = S; W[Y] = W[X] + 1; q.push(Y); }
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, M, R, x, y; cin >> N >> M >> R;
+	long long P = 0;
+	for (int n = 0; n < M; n++) {
+		cin >> x >> y; W[n + 1] = -1;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	for (int n = 1; n <= N; n++) { sort(v[n].begin(), v[n].end()); }
+	B(R); M = 0;
+	for (int n = 1; n <= N; n++) {
+		if (W[n] != -1) { P += ((long long)w[n] * W[n]); M++; if (M == S) { break; } }
+	}
+	cout << P;
+	return 0;
+}
