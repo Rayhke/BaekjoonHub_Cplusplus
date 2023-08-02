2710b0aafb1c42b38555c1fc4424787bcde01f4b
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-02 17:41:45 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2676 ms, Memory: 3232 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector <int> v[10001];
+int w[10001], W = 0;
+bool r[10001];
+
+void V(int S) {
+	int P = 0; queue <int> q; q.push(S); r[S] = 1;
+	while (!q.empty()) {
+		int X = q.front(); q.pop();
+		for (int n = 0; n < v[X].size(); n++) {
+			int Y = v[X][n];
+			if (!r[Y]) { r[Y] = 1; P++; q.push(Y); }
+		}
+	}
+	w[S] = P;
+	W = (P > W) ? P : W;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	int N, M, x, y; cin >> N >> M;
+	for (int n = 0; n < M; n++) { cin >> y >> x; v[x].push_back(y); }
+	for (int n = 1; n <= N; n++) { V(n); fill(r, r + 10002, 0); }
+	for (int n = 1; n <= N; n++) { if (w[n] < W) { continue; } cout << n << ' '; }
+	return 0;
+}
