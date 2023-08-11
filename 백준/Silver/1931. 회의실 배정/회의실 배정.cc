ca1afc7be31ac1fc3a9caa281ffdd253d68816c0
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-12 01:45:48 +0900

    [Silver I] Title: 회의실 배정, Time: 28 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	vector <pair<int, int>> v;
+	int N, M = 0, L = 1, P = 1; cin >> N;
+	for (int n = 0; n < N; n++) {
+		int s, e; cin >> s >> e;
+		v.push_back({ e,s });
+	}
+	sort(v.begin(), v.end());
+	if (N > 1) {
+		while (L != N) {
+			if (v[M].first <= v[L].second) { P++; M = L; L = M + 1; }
+			else { L++; }
+		}
+	}
+	cout << P;
+	return 0;
+}
