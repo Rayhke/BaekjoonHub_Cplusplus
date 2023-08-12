bf4764ec4b59231f0e99e89ab1b68eff8dd8c16a
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-12 23:10:02 +0900

    [Silver II] Title: 좌표 압축, Time: 1380 ms, Memory: 35824 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	int N, M, L = 0;
+	vector <int> v; map <int, int> mp; cin >> N;
+	for (int n = 0; n < N; n++) {
+		cin >> M; v.push_back(M);
+		if (!mp.count(M)) { mp.insert({ M,0 }); }
+	}
+	for (auto& n : mp) { n.second = L; L++; }
+	for (int n = 0; n < N; n++) { cout << mp[v[n]] << ' '; }
+}
