b198409588cf9e4081ca2caa75e84b7a8538c196
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-12 21:36:23 +0900

    Update 최소， 최대_2.cc

--- 백준/Bronze/10818. 최소， 최대/최소， 최대_2.cc
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대_2.cc
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	ios::sync_with_stdio(false);
+	int N, M, P = 1e6, C = -1e6; cin >> N;
+	for (int n = 0; n < N; n++) {
+		cin >> M;
+		P = (M < P) ? M : P; C = (M > C) ? M : C;
+	}
+	cout << P << ' ' << C;
+	return 0;
+}
