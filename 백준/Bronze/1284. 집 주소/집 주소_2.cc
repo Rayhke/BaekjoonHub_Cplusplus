4c0a957ab2774cce02b4a1c99200eac7fd8908b7
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-14 17:03:11 +0900

    Update 집 주소_2.cc

--- 백준/Bronze/1284. 집 주소/집 주소_2.cc
+++ 백준/Bronze/1284. 집 주소/집 주소_2.cc
@@ -1,1 +1,14 @@
+#include <iostream>
+#include <map>
+using namespace std;
 
+int main() {
+	int N; string s;
+	map <char, int> mp = { {'0',4},{'1',2},{'2',3},{'3',3},{'4',3},{'5',3},{'6',3},{'7',3},{'8',3},{'9',3} };
+	while (1) {
+		cin >> s; N = 1;
+		if (s == "0") { break; }
+		for (int n = 0; s[n] != '\0'; n++) { N += mp[s[n]] + 1; }
+		cout << N << '\n';
+	}
+}
