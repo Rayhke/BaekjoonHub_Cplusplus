15d0fbe59f3ce9fc12d0bae0ba8b05ff1c776686
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-20 00:21:51 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N; cin >> N; vector <int> v(N), w; w.push_back(1001);
+	for (int n = 0; n < N; n++) { cin >> v[n]; }
+	for (int n = 0; n < N; n++) {
+		if (w[w.size() - 1] > v[n]) { w.push_back(v[n]); }
+		else { w[lower_bound(w.begin(), w.end(), v[n], greater()) - w.begin()] = v[n]; }
+	}
+	cout << w.size() - 1;
+	return 0;
+}
