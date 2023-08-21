e2025affc5eee7937d29e16c610403377a257d7d
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-21 09:59:02 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 8 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, U = 1e9, P = 1, v[125][125], A[] = { 1,-1,0,0 }, B[] = { 0,0,1,-1 };
+
+
+void V() {
+	vector <vector<int>> w(N, vector <int>(N, U)); w[0][0] = v[0][0];
+	queue <pair<int, int>> q; q.push({ 0,0 });
+	while (!q.empty()) {
+		int x = q.front().first, y = q.front().second; q.pop();
+		for (int n = 0; n < 4; n++) {
+			int X = x + A[n], Y = y + B[n];
+			if (-1 < X && X < N && -1 < Y && Y < N) {
+				if (w[Y][X] > v[Y][X] + w[y][x]) {
+					w[Y][X] = v[Y][X] + w[y][x]; q.push({ X,Y });
+				}
+			}
+		}
+	}
+	cout << "Problem " << P << ": " << w[N - 1][N - 1] << '\n'; P++;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	while (1) {
+		cin >> N; if (N == 0) { break; }
+		for (int n = 0; n < N; n++) {
+			for (int m = 0; m < N; m++) { cin >> v[n][m]; }
+		}
+		V();
+	}
+	return 0;
+}
