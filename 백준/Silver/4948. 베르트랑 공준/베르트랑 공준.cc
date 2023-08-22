2d915a27d946c29f71a053f9c1d9c93b0d9b4d07
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-22 21:56:02 +0900

    Update 베르트랑 공준.cc

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -5,13 +5,13 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	vector <bool> r(246913, 1);
-	int N, M, P = 0;
+	int N, P = 0;
 	for (int n = 2; n * n < 246913; n++) {
 		if (r[n]) { for (int m = n * n; m < 246913; m += n) { r[m] = 0; } }
 	}
 	while (1) {
-		cin >> M; if (M == 0) { break; }
-		for (int n = M + 1; n < M * 2 + 1; n++) { if (r[n]) { P++; } }
+		cin >> N; if (N == 0) { break; }
+		for (int n = N + 1; n < N * 2 + 1; n++) { if (r[n]) { P++; } }
 		cout << P << '\n'; P = 0;
 	}
 	return 0;
