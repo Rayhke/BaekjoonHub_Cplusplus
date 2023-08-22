2609b26c2291ed9155386b7535fad7ffd115efae
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-22 22:01:12 +0900

    Update 베르트랑 공준.cc

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
-	vector <bool> r(246913, 1);
+	vector <bool> r(246913, 1);	// r[1] = 0; 을 넣어야 하지만, 이 문제의 특징 덕에 큰 문제는 없을 것이다.
 	int N, P = 0;
 	for (int n = 2; n * n < 246913; n++) {
 		if (r[n]) { for (int m = n * n; m < 246913; m += n) { r[m] = 0; } }
