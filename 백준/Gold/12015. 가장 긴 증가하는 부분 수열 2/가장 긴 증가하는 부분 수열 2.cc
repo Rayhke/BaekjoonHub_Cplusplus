ebe8bd4c2c946feba366a388cab5ad9a510ca32a
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-26 07:26:31 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 156 ms, Memory: 12200 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int N; cin >> N; vector <int> v(N), w; w.push_back(0);
+	for (int n = 0; n < N; n++) { cin >> v[n]; }
+	for (int n = 0; n < N; n++) {
+		if (w[w.size() - 1] < v[n]) { w.push_back(v[n]); }
+		else { w[lower_bound(w.begin(), w.end(), v[n]) - w.begin()] = v[n]; }
+	}
+	cout << w.size() - 1;
+	return 0;
+}
