1c8f7bdb8c3acb335d13cedb238a40179377035d
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-08-27 19:01:07 +0900

    Update 가장 긴 증가하는 부분 수열 3.cc

--- 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -1,16 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std; 
+using namespace std;
 
 int main() {
-ios::sync_with_stdio(false); cin.tie(NULL);
-int N; cin >> N; vector <int> v(N), w; w.push_back(-1000000001);
-for (int n = 0; n < N; n++) { cin >> v[n]; }
-for (int n = 0; n < N; n++) {
-if (w[w.size() - 1] < v[n]) { w.push_back(v[n]); }
-else { w[lower_bound(w.begin(), w.end(), v[n]) - w.begin()] = v[n]; }
-}
-cout << w.size() - 1;
-return 0;
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int N, U = 1e9 + 1; cin >> N; vector <int> v(N), w; w.push_back(-U);
+	cout << w[0] << '\n';
+	for (int n = 0; n < N; n++) { cin >> v[n]; }
+	for (int n = 0; n < N; n++) {
+		if (w[w.size() - 1] < v[n]) { w.push_back(v[n]); }
+		else { w[lower_bound(w.begin(), w.end(), v[n]) - w.begin()] = v[n]; }
+	}
+	cout << w.size() - 1;
+	return 0;
 }
