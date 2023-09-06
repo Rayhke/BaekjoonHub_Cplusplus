a6342181ef8033cfc09e24f73a8a9666e660f533
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-09-06 14:45:55 +0900

    [Silver II] Title: A → B, Time: 0 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	bool R = 1; int A, B, U = 1e9 + 1; cin >> A >> B;
+	queue <pair<long long, int>> q; q.push({ A,1 });
+	while (!q.empty()) {
+		int P = q.front().second; long long X = q.front().first; q.pop();
+		if (X > B) { continue; }
+		if (X == B) { cout << P; R = 0; break; }
+		if (X * 2 < U) { q.push({ X * 2,P + 1 }); }
+		if (X * 10 + 1 < U) { q.push({ X * 10 + 1,P + 1 }); }
+	}
+	if (R) { cout << -1; }
+	return 0;
+}
