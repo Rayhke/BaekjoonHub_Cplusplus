9b6de7a40bd4d07fd9135f3f7858f5f8f9bcfacd
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-09-10 10:36:17 +0900

    Create N과 M （2）_2.cc

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）_2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool r[9];
+int N, M, v[9];
+
+void V(int W, int P) {
+	if (P == M) {
+		for (int n = 0; n < M; n++) { cout << v[n] << ' '; }
+		cout << '\n'; return;
+	}
+	for (int n = W; n <= N; n++) {
+		if (r[n]) {
+			r[n] = 0; v[P] = n;
+			V(n + 1, P + 1); r[n] = 1;
+		}
+	}
+}
+
+int main() {
+	for (int n = 1; n < 9; n++) { r[n] = 1; }
+	cin >> N >> M; V(1, 0);
+	return 0;
+}
