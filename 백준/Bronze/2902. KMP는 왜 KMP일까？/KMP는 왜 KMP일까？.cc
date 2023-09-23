b33f7577a128591cbf3025e71e194155d9be598c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-09-23 20:28:22 +0900

    Update KMP는 왜 KMP일까？.cc

--- 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
+++ 백준/Bronze/2902. KMP는 왜 KMP일까？/KMP는 왜 KMP일까？.cc
@@ -3,10 +3,13 @@ using namespace std;
 
 int main() {
 	string s, p = ""; cin >> s;
-	for (int n = 0; n < s.length(); n++) {
+	for (int n = 0; n < s.length(); n++) {			// s.size(); 도 가능하긴하다.
 		if (s[n] < 65 || 90 < s[n]) { continue; }
 		p += s[n];
 	}
 	cout << p;
 	return 0;
 }
+/* ■ 설명 ■
+문자열 변수.length() 를 사용하려면 헤더 <string> 을 선언하는 것이 정석이다.
+*/
