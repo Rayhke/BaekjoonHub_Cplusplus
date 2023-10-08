5bd3d29b259e84f86fb27b8f26ba210dda4a5395
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-10-08 12:15:03 +0900

    Update 알파벳.cc

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -5,17 +5,15 @@ bool w[26];
 char v[20][21];
 int R, C, P = 1, PX[] = { 1,-1,0,0 }, PY[] = { 0,0,1,-1 };
 
-void B(int N, int M, int K) {
-	char W = v[N][M];
-	w[W - 65] = 1;
-	P = max(P, K);
+void B(int N, int M, int K, char c) {
+	P = max(P, K); w[c - 65] = 1;
 	for (int n = 0; n < 4; n++) {
 		int X = M + PX[n], Y = N + PY[n];
-		W = v[Y][X];
-		if (X < 0 || C <= X || Y < 0 || R <= Y || w[W - 65]) { continue; }
-		B(Y, X, K + 1);
+		char V = v[Y][X];
+		if (X < 0 || C <= X || Y < 0 || R <= Y || w[V - 65]) { continue; }
+		B(Y, X, K + 1, V);
 	}
-	w[v[N][M] - 65] = 0;
+	w[c - 65] = 0;
 }
 
 int main() {
@@ -24,7 +22,7 @@ int main() {
 	for (int n = 0; n < R; n++) {
 		for (int m = 0; m < C; m++) { cin >> v[n][m]; }
 	}
-	B(0, 0, 1);
+	B(0, 0, 1, v[0][0]);
 	cout << P;
 	return 0;
 }
