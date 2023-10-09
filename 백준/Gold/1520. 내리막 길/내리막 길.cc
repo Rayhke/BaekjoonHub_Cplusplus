b1ef38e4016d852f92db31933161046f0b889b5e
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-10-09 17:36:17 +0900

    [Gold III] Title: 내리막 길, Time: 24 ms, Memory: 3728 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.cc
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -2,14 +2,15 @@
 using namespace std;
 
 bool r[500][500];
-int M, N, v[500][500], w[500][500], PX[] = { 1,-1,0,0 }, PY[] = { 0,0,1,-1 };
+short M, N, v[500][500], PX[] = { 1,-1,0,0 }, PY[] = { 0,0,1,-1 };
+int w[500][500];
 
-int V(int y, int x) {
+int V(short y, short x) {
 	if (y == M - 1 && x == N - 1) { return 1; }
 	if (r[y][x]) { return w[y][x]; }
 	r[y][x] = 1;
 	for (int n = 0; n < 4; n++) {
-		int X = x + PX[n], Y = y + PY[n];
+		short X = x + PX[n], Y = y + PY[n];
 		if (X < 0 || N <= X || Y < 0 || M <= Y) { continue; }
 		if (v[y][x] > v[Y][X]) { w[y][x] += V(Y, X); }
 	}
@@ -17,7 +18,7 @@ int V(int y, int x) {
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false); cin.tie(NULL);
 	cin >> M >> N;
 	for (int m = 0; m < M; m++) {
 		for (int n = 0; n < N; n++) { cin >> v[m][n]; }
