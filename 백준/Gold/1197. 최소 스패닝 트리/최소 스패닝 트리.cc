18fb42304627854fbc328e7b98799ad629bcfc2c
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-10-14 14:33:00 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 44 ms, Memory: 4492 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int V, E, v[10001];
+priority_queue <pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+
+int find(int n) {
+	if (n != v[n]) { v[n] = find(v[n]); }
+	return v[n];
+}
+
+void W() {
+	int P = 0;
+	while (!pq.empty()) {
+		int X = pq.top().second.first, Y = pq.top().second.second, Z = pq.top().first; pq.pop();
+		int S = find(X), E = find(Y);
+		if (S == E) { continue; }
+		v[E] = S; P += Z;
+	}
+	cout << P;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	int x, y, z; cin >> V >> E;
+	for (int n = 1; n <= V; n++) { v[n] = n; }
+	for (int n = 0; n < E; n++) {
+		cin >> x >> y >> z;
+		pq.push({ z,{x,y} });
+	}
+	W();
+	return 0;
+}
