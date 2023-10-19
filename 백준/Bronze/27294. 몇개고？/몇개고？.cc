021a6301ebab35c31ca141fffe6921822fcc4fcb
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-10-19 11:50:38 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool S; int T; cin >> T >> S;
+	if (T < 12) { cout << 280; }
+	else if (T < 17) { (S) ? cout << 280 : cout << 320; }
+	else { cout << 280; }
+	return 0;
+}
