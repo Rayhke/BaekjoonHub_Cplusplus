fa3d486aebba594890210b3fe52eb90b3da1fbf2
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-10-27 22:48:29 +0900

    [Gold III] Title: 레이저 통신, Time: 4 ms, Memory: 2748 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.cc
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -10,25 +10,25 @@ short H, W, HE, WE;
 int U = 1e9;
 PairSS PM[] = { {0,1},{0,-1},{1,0},{-1,0} };
 vector <vector<vector<int>>> v(100, vector<vector<int>>(100, vector<int>(4, U)));
-priority_queue <pair<PairIS, PairSS>, vector<pair<PairIS, PairSS>>, greater<pair<PairIS, PairSS>>> pq;
+queue <pair<PairIS, PairSS>> q;
 
 void V() {
-	while (!pq.empty()) {
-		short x = pq.top().second.second, y = pq.top().second.first, z = pq.top().first.second;
-		int P = pq.top().first.first; pq.pop();
+	while (!q.empty()) {
+		short x = q.front().second.second, y = q.front().second.first, z = q.front().first.second;
+		int P = q.front().first.first; q.pop();
 		for (int n = 0; n < 4; n++) {
 			short X = x + PM[n].second, Y = y + PM[n].first;
 			if (X < 0 || W <= X || Y < 0 || H <= Y || r[Y][X]) { continue; }
 			if (P != -1) {
 				if (z != n) {
-					if (v[Y][X][n] > P + 1) { m[Y][X][n] = 1; v[Y][X][n] = P + 1; pq.push({ {P + 1,n},{Y,X} }); }
+					if (v[Y][X][n] > P + 1) { m[Y][X][n] = 1; v[Y][X][n] = P + 1; q.push({ {P + 1,n},{Y,X} }); }
 				}
 				else {
-					if (v[Y][X][n] > P) { m[Y][X][n] = 1; v[Y][X][n] = P; pq.push({ {P,n},{Y,X} }); }
+					if (v[Y][X][n] > P) { m[Y][X][n] = 1; v[Y][X][n] = P; q.push({ {P,n},{Y,X} }); }
 				}
 			}
 			else {
-				if (v[Y][X][n] > P) { m[Y][X][n] = 1; v[Y][X][n] = P; pq.push({ {P,n},{Y,X} }); }
+				if (v[Y][X][n] > P) { m[Y][X][n] = 1; v[Y][X][n] = P; q.push({ {P,n},{Y,X} }); }
 			}
 
 		}
@@ -48,7 +48,7 @@ int main() {
 			if (s[w] == 42) { r[h][w] = 1; }
 			else if (s[w] == 67) {
 				if (R) {
-					pq.push({ {0,-1},{h,w} }); R = 0;
+					q.push({ {0,-1},{h,w} }); R = 0;
 					for (int n = 0; n < 4; n++) { v[h][w][n] = 0; m[h][w][n] = 1; }
 				}
 				else { HE = h, WE = w; }
