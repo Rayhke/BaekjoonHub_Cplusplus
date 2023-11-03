b65dc807bc5259819445be0cd5eac1278e234fde
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-03 11:02:36 +0900

    [Gold III] Title: LCA, Time: 1628 ms, Memory: 7240 KB -BaekjoonHub

---
+++ 백준/Gold/11437. LCA/LCA.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#define US unsigned short
+using namespace std;
+
+bool R[50001];
+US N, M;
+pair <US, US> w[50001];
+vector <US> v[50001];
+
+void D(int X, int Y) {
+	R[X] = 1; w[X].second = Y;
+	for (const auto& n : v[X]) {
+		if (R[n]) { continue; }
+		w[n].first = X; D(n, Y + 1);
+	}
+}
+
+int LCA(int N1, int N2) {
+	if (N1 == N2) { return N1; }
+	if (w[N1].second == w[N2].second) { return LCA(w[N1].first, w[N2].first); }
+	else if (w[N1].second > w[N2].second) { return LCA(w[N1].first, N2); }
+	else { return LCA(N1, w[N2].first); }
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	US x, y; cin >> N;
+	for (US n = 0; n < N - 1; n++) {
+		cin >> x >> y;
+		v[x].push_back(y);
+		v[y].push_back(x);
+	}
+	D(1, 0);
+	cin >> M;
+	for (US m = 0; m < M; m++) {
+		cin >> x >> y; cout << LCA(x, y) << '\n';
+	}
+	return 0;
+}
