1cbaf9af92c5eabe30c3482f2c670ad25afb1716
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-09 16:06:35 +0900

    [Gold III] Title: 줄 세우기, Time: 28 ms, Memory: 3988 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -3,9 +3,10 @@
 #include <queue>
 using namespace std;
 
+
 bool R[32001];
-int N, M;
-vector <int> v[32001], w(32001);
+int N, M, w[32001];
+vector <int> v[32001];
 queue <int> q;
 
 void V() {
@@ -19,11 +20,10 @@ void V() {
 }
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false); cout.tie(NULL);
 	int x, y; cin >> N >> M;
 	for (int m = 0; m < M; m++) {
-		cin >> x >> y;
-		w[y]++; v[x].push_back(y);
+		cin >> x >> y; w[y]++; v[x].push_back(y);
 	}
 	for (int n = 1; n <= N; n++) {
 		if (w[n] == 0 && !R[n]) { q.push(n); V(); }
