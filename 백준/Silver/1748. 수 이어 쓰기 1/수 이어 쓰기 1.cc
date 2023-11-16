3699197ce3d64b222d49451048e6fe30cbe7926e
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-17 08:54:20 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int N, M = 1e8, L = 9, P = 0; cin >> N;
+	while (1) {
+		if (M <= N) { P += (N - M + 1) * L; break; }
+		M /= 10; L--;
+	}
+	M /= 10; L--;
+	for (int m = M; m > 0; m /= 10) { P += 9 * m * L; L--; }
+	cout << P;
+	return 0;
+}
