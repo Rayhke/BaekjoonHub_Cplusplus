79732706d6f8b42ca14a5c353a30c3ad69be15f2
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-20 12:39:39 +0900

    [Gold III] Title: 허들 넘기, Time: 1484 ms, Memory: 2444 KB -BaekjoonHub

---
+++ 백준/Gold/23286. 허들 넘기/허들 넘기.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#define Vec vector<int>
+using namespace std;
+
+short N, M;
+int T, U = 1e9;
+vector <Vec> v(301, Vec(301, U));
+
+int main() {
+	ios::sync_with_stdio(false); cout.tie(NULL);
+	int x, y, z; cin >> N >> M >> T;
+	for (int m = 0; m < M; m++) {
+		cin >> x >> y >> z;
+		if (v[x][y] > z) { v[x][y] = z; }
+	}
+	for (int n = 1; n <= N; n++) { v[n][n] = 0; }
+	for (int m = 1; m <= N; m++) {
+		for (int s = 1; s <= N; s++) {
+			for (int e = 1; e <= N; e++) {
+				if (v[s][e] > v[s][m] || v[s][e] > v[m][e]) {
+					v[s][e] = min(v[s][e], max(v[s][m], v[m][e]));
+				}
+			}
+		}
+	}
+	for (int t = 0; t < T; t++) {
+		cin >> x >> y;
+		if (v[x][y] != U) { cout << v[x][y] << '\n'; }
+		else { cout << -1 << '\n'; }
+	}
+	return 0;
+}
