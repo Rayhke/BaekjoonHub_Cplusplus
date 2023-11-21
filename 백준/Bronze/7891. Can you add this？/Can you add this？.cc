1bac6a205948c460d6850740744a69d03f469094
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-21 13:53:51 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	int T, X, Y; cin >> T;
+	while (T--) {
+		cin >> X >> Y;
+		cout << X + Y << '\n';
+	}
+	return 0;
+}
