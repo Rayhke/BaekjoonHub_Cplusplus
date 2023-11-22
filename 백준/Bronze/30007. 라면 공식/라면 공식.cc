c3bee1aec1b1ed80dcaf1011b7d774c370977724
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-22 18:34:37 +0900

    [Bronze V] Title: 라면 공식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	int N, A, B, X; cin >> N;
+	while (N--) {
+		cin >> A >> B >> X;
+		cout << A * (X - 1) + B << '\n';
+	}
+	return 0;
+}
