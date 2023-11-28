d2f3d496323db84768c8e98d3c87ff5d955f9612
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-28 10:23:01 +0900

    [Gold II] Title: 문제집, Time: 28 ms, Memory: 3964 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int N, M, w[32001];
+vector <int> v[32001];
+priority_queue <int, vector<int>, greater<int>> pq;
+
+void V() {
+	while (!pq.empty()) {
+		int X = pq.top(); pq.pop();
+		cout << X << ' ';
+		for (const auto& n : v[X]) {
+			if (--w[n] == 0) { pq.push(n); }
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	int x, y; cin >> N >> M;
+	while (M--) {
+		cin >> x >> y; w[y]++;
+		v[x].push_back(y);
+	}
+	for (int n = 1; n <= N; n++) {
+		if (w[n] == 0) { pq.push(n); }
+	}
+	V();
+	return 0;
+}
