ea6034f77535a251a62b1ce16fa0dd2a1a9842ba
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-11-29 14:48:11 +0900

    [Gold II] Title: 자동차경주, Time: 128 ms, Memory: 7448 KB -BaekjoonHub

---
+++ 백준/Gold/2611. 자동차경주/자동차경주.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#define PR pair<int, int>
+using namespace std;
+
+int N, M, P = 1, W[1001];
+PR w[1001];
+vector <PR> v[1001];
+queue <PR> q;
+stack <int> s;
+
+void V() {
+	while (!q.empty()) {
+		int X = q.front().second, Q = q.front().first; q.pop();
+		if (w[X].second > Q) { continue; }
+		for (const auto& n : v[X]) {
+			if (w[n.second].second < w[X].second + n.first) {
+				w[n.second].second = w[X].second + n.first; W[n.second] = X;
+			}
+			if (--w[n.second].first == 0) {
+				q.push({ w[n.second].second,n.second });
+			}
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int x, y, z; cin >> N >> M;
+	while (M--) {
+		cin >> x >> y >> z; w[y].first++;
+		v[x].push_back({ z,y });
+	}
+	w[P].first = 0; s.push(P); q.push({ 0,P }); V();
+	cout << w[1].second << '\n';
+	while (1) {
+		s.push(W[P]);
+		if (W[P] == 1) { break; }
+		P = W[P];
+	}
+	while (!s.empty()) {
+		cout << s.top() << ' '; s.pop();
+	}
+	return 0;
+}
