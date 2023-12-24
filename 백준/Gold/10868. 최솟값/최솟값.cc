a6a85c9f57da0b877cb850135cbb6fa277abd985
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-12-24 21:25:05 +0900

    Update 최솟값.cc

--- 백준/Gold/10868. 최솟값/최솟값.cc
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-void init(vector<int>& v, vector<int>& tree, int node, int start, int end) {
+void init(vector<int>& v, vector<int>& tree, int node, int start, int end) {		// 핵심 부분 (어떻게 구조를 만드냐에 따라 다양한 문제에 대응함)
 	if (start == end) { tree[node] = v[start]; }
 	else {
 		init(v, tree, node * 2, start, (start + end) / 2);
@@ -12,7 +12,7 @@ void init(vector<int>& v, vector<int>& tree, int node, int start, int end) {
 	}
 }
 
-int query(vector<int>& tree, int node, int start, int end, int left, int right) {
+int query(vector<int>& tree, int node, int start, int end, int left, int right) {	// 마찬가지로 어떻게 탐색 하느냐에 따라, 다양한 대응이 가능하다.
 	if (right < start || end < left) { return 0; }
 	if (left <= start && end <= right) { return tree[node]; }
 	int Lmin = query(tree, node * 2, start, (start + end) / 2, left, right);
@@ -25,8 +25,8 @@ int query(vector<int>& tree, int node, int start, int end, int left, int right) 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL);
 	int N, M, H, tree_size, L, R; cin >> N >> M;
-	H = (int)ceil(log2(N));
-	tree_size = (1 << (H + 1));
+	H = (int)ceil(log2(N));				// [중요]
+	tree_size = (1 << (H + 1));			// [중요]
 	vector <int> v(N);
 	vector <int> tree(tree_size);
 	for (int n = 0; n < N; n++) { cin >> v[n]; }
@@ -37,3 +37,6 @@ int main() {
 	}
 	return 0;
 }
+/* ■ 설명 ■
+세그먼트 트리 (추가 설명 적을 것)
+*/
