4262096d8cfa3b089687c685256b9230e4a902a7
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-12-24 21:31:46 +0900

    Update 최솟값.cc

--- 백준/Gold/10868. 최솟값/최솟값.cc
+++ 백준/Gold/10868. 최솟값/최솟값.cc
@@ -4,8 +4,8 @@
 using namespace std;
 
 void init(vector<int>& v, vector<int>& tree, int node, int start, int end) {		// 핵심 부분 (어떻게 구조를 만드냐에 따라 다양한 문제에 대응함)
-	if (start == end) { tree[node] = v[start]; }				// 끝에 도달하면 그것에 맞는 v배열의 원소 대입
-	else {									// 오른쪽, 왼쪽으로 가장 아래 끝까지 깊게 내려가기
+	if (start == end) { tree[node] = v[start]; }				// 이진 트리의 끝에 도달하면 그것에 맞는 v배열의 원소 대입
+	else {									// 이진 트리의 오른쪽, 왼쪽의 가장 아래 끝까지 깊게 내려가기
 		init(v, tree, node * 2, start, (start + end) / 2);
 		init(v, tree, node * 2 + 1, (start + end) / 2 + 1, end);
 		tree[node] = min(tree[node * 2], tree[node * 2 + 1]);		// 이게 핵심, 다시 올라오면서, v배열에 대입 했던 범위 원소에서 가장 작은 값을 위로 올리기
@@ -13,8 +13,8 @@ void init(vector<int>& v, vector<int>& tree, int node, int start, int end) {		//
 }
 
 int query(vector<int>& tree, int node, int start, int end, int left, int right) {	// 마찬가지로 어떻게 탐색 하느냐에 따라, 다양한 대응이 가능하다.
-	if (right < start || end < left) { return 0; }
-	if (left <= start && end <= right) { return tree[node]; }
+	if (right < start || end < left) { return 0; }				// 범위 바깥으로 탐색을 시도 시, 탈락
+	if (left <= start && end <= right) { return tree[node]; }		// 정확하게 범위 내에 들어 왔다면, 값을 가지고 이진 트리 위로 올라가기
 	int Lmin = query(tree, node * 2, start, (start + end) / 2, left, right);
 	int Rmin = query(tree, node * 2 + 1, (start + end) / 2 + 1, end, left, right);
 	if (Lmin == 0) { return Rmin; }
