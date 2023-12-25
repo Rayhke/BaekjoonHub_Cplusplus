205f36346ad17ade4f4230083fbbe8f9e7594430
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-12-25 19:33:55 +0900

    [Gold I] Title: Beads, Time: 76 ms, Memory: 3056 KB -BaekjoonHub

---
+++ 백준/Gold/11143. Beads/Beads.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int tree[262144];
+
+int update(int node, int start, int end, int index, int value) {
+	if (index < start || end < index) { return tree[node]; }
+	if (start == end) { return tree[node] += value; }
+	int mid = (start + end) / 2;
+	return tree[node] = update(node * 2, start, mid, index, value) + update(node * 2 + 1, mid + 1, end, index, value);
+}
+
+int query(int node, int start, int end, int left, int right) {
+	if (right < start || end < left) { return 0; }
+	if (left <= start && end <= right) { return tree[node]; }
+	int mid = (start + end) / 2;
+	return query(node * 2, start, mid, left, right) + query(node * 2 + 1, mid + 1, end, left, right);
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	char C; int T, B, P, Q, H, tree_size, L, R; cin >> T;
+	while (T--) {
+		cin >> B >> P >> Q; P += Q;
+		H = (int)ceil(log2(B));
+		tree_size = (1 << (H + 1));
+		while (P--) {
+			cin >> C >> L >> R;
+			if (C == 80) { update(1, 0, B - 1, L - 1, R); }
+			else {
+				cout << query(1, 0, B - 1, L - 1, R - 1) << '\n';
+			}
+		}
+		fill_n(tree, tree_size, 0);
+	}
+	return 0;
+}
