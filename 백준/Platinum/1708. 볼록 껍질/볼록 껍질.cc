e74205d1f2ec2e9f5443ce8fdc2bd783cc80bd70
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2023-12-28 14:50:53 +0900

    Update 볼록 껍질.cc

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cc
@@ -3,7 +3,7 @@
 #include <stack>
 #include <algorithm>
 #define LL long long
-#define PAIR pair<LL, LL>
+#define PAIR pair<LL, LL>	// first : y축, second : x축
 using namespace std;
 
 vector <PAIR> v;
@@ -17,11 +17,11 @@ LL CCW(PAIR& P1, PAIR& P2, PAIR& P3) {
 
 bool P(PAIR& P2, PAIR& P3) {
 	LL M = CCW(v[0], P2, P3);
-	if (M == 0) {
+	if (M == 0) {								// 점 위치가 서로 평행 한다면, 길이가 더 짧은 쪽이 앞으로 오도록 정렬
 		if (P2.first != P3.first) { return P2.first < P3.first; }
 		return P2.second < P3.second;
 	}
-	return M > 0;
+	return M > 0;								// 반시계를 이루는 점부터 앞으로 오도록 정렬
 }
 
 int main() {
@@ -31,17 +31,17 @@ int main() {
 		cin >> x >> y;
 		v.push_back({ y,x });
 	}
-	sort(v.begin(), v.end());
+	sort(v.begin(), v.end());			// 특별히 정렬 추가 조건을 안 붙여도 되는 이유가 y, x 원소 순으로 이루어짐
 	sort(v.begin() + 1, v.end(), P);
 	for (auto& n : v) {
 		while (s.size() > 1) {
 			PAIR P2 = s.top(); s.pop();
 			PAIR P1 = s.top();
 			if (CCW(P1, P2, n) > 0) {
-				s.push(P2); break;
+				s.push(P2); break;	// CCW 조건에 성립 하므로, 중간 점은 남겨둠
 			}
 		}
-		s.push(n);
+		s.push(n);				// 여기로 도달했단 것은 앞의 CCW 조건에 성립하여, 통과했단 의미이므로 끝 점을 넣어주는 작업
 	}
 	cout << s.size();
 	return 0;
