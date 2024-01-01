43ac257fb3c464d53e1d6c181909d10edfb57a27
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-01-01 20:41:01 +0900

    [Platinum V] Title: Cows, Time: 4 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Platinum/6850. Cows/Cows.cc
+++ 백준/Platinum/6850. Cows/Cows.cc
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define LL long long
-#define PAIR pair<LL, LL>
+#define PAIR pair<int, int>
 using namespace std;
 
 vector <PAIR> v, w;
 
-LL CCW(PAIR& P1, PAIR& P2, PAIR& P3) {
-	LL M = P1.first * P2.second + P2.first * P3.second + P3.first * P1.second;
+int CCW(PAIR& P1, PAIR& P2, PAIR& P3) {
+	int M = P1.first * P2.second + P2.first * P3.second + P3.first * P1.second;
 	M -= P1.first * P3.second + P2.first * P1.second + P3.first * P2.second;
 	return M;
 }
 
 bool P(PAIR& P2, PAIR& P3) {
-	LL M = CCW(v[0], P2, P3);
+	int M = CCW(v[0], P2, P3);
 	if (M == 0) {
 		if (P2.first != P3.first) { return P2.first < P3.first; }
 		return P2.second < P3.second;
@@ -24,7 +23,7 @@ bool P(PAIR& P2, PAIR& P3) {
 
 int main() {
 	ios::sync_with_stdio(false);
-	int N, R; LL x, y; double Area = 0; cin >> N;
+	int N, x, y, Area = 0; cin >> N;
 	for (int n = 0; n < N; n++) {
 		cin >> x >> y;
 		v.push_back({ x,y });
@@ -42,9 +41,8 @@ int main() {
 		w.push_back(n);
 	}
 	for (int n = 1; n < w.size() - 1; n++) {
-		Area += CCW(w[0], w[n], w[n + 1]) / (double)2;
+		Area += CCW(w[0], w[n], w[n + 1]) / 2;
 	}
-	R = abs(Area) / 50;
-	cout << R;
+	cout << abs(Area) / 50;
 	return 0;
 }
