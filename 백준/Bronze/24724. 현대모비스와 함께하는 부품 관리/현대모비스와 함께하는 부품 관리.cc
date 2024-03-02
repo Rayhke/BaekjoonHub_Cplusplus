0299fd690e8eab5502095753bfa2f2efee7232c9
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-03-03 00:15:05 +0900

    Update 현대모비스와 함께하는 부품 관리.cc

--- 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	ios::sync_with_stdio(false); cin.tie(NULL);
 	int T; cin >> T;
 	for (int n = 1; n <= T; n++) {
 		printf("Material Management %d\nClassification ---- End!\n", n);
