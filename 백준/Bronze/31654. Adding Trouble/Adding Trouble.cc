44af3d01e5cbdfdb429271e9ac96ddf3a49eded0
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-03-22 14:31:15 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long A, B, C; cin >> A >> B >> C;
+	if (A + B != C) { cout << "wrong!"; }
+	else { cout << "correct!"; }
+	return 0;
+}
