78701b2e2e5732deb20262c69f9ca19847cda549
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-04-17 15:48:44 +0900

    [Bronze IV] Title: No Brainer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4562. No Brainer/No Brainer.cc
+++ 백준/Bronze/4562. No Brainer/No Brainer.cc
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main() {
-	vector <bool> V;
 	int N, X, Y; cin >> N;
 	while (N--) {
 		cin >> X >> Y;
-		V.push_back((X < Y) ? 0 : 1);
-	}
-	for (bool R : V) {
-		(R) ? cout << "MMM" : cout << "NO";
+		(X < Y) ? cout << "NO" : cout << "MMM";
 		cout << " BRAINS\n";
 	}
 	return 0;
