41c1498caf2a2ffc1abbab93c53ac43dbd67ed85
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-04-28 14:28:51 +0900

    [Silver IV] Title: 온라인 판매, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int N, M, P[1001], Min = 1, Max = 0, Value; cin >> N >> M;
+	for (int m = 0; m < M; m++) { cin >> P[m]; }
+	sort(P, P + M, greater<int>());
+	Value = P[0];
+	for (int m = 1; m < M; m++) {
+		if (m >= N) { break; }
+		Value = P[m] * (m + 1);
+		if (Max <= Value) { Max = Value; Min = P[m]; }
+	}
+	cout << Min << ' ' << Max;
+	return 0;
+}
