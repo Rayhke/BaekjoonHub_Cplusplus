2ad178f3c72bdc539a4ee3baa55c8107086dcf42
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-06-06 21:52:51 +0900

    [Bronze III] Title: 쿠키애호가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11134. 쿠키애호가/쿠키애호가.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	int T, N, C, day; cin >> T;
+	while (T--) {
+		cin >> N >> C;
+		day = N / C + ((N % C != 0) ? 1 : 0);
+		cout << day << '\n';
+	}
+	return 0;
+}
