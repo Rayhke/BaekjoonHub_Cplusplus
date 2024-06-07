3dd082275f70441231845059bf1c747900983ae3
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-06-07 11:08:36 +0900

    [Bronze IV] Title: Случай с игрой, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29267. Случай с игрой/Случай с игрой.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+	int N, K, Bullet = 0, SaveBullet = 0; string Action; cin >> N >> K;
+	while (N--) {
+		cin >> Action;
+		if (Action == "save") { SaveBullet = Bullet; }
+		else if (Action == "load") { Bullet = SaveBullet; }
+		else if (Action == "shoot") { Bullet--; }
+		else if (Action == "ammo") { Bullet += K; }
+		cout << Bullet << '\n';
+	}
+	return 0;
+}
