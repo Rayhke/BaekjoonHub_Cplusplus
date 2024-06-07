9b34e6c4c65de5eaafbb1a8d67b39a6ad0035e04
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-06-07 11:11:50 +0900

    Update Случай с игрой.cc

--- 백준/Bronze/29267. Случай с игрой/Случай с игрой.cc
+++ 백준/Bronze/29267. Случай с игрой/Случай с игрой.cc
@@ -2,15 +2,15 @@
 using namespace std;
 
 int main() {
-    cin.tie(NULL);
-    int N, K, Bullet = 0, SaveBullet = 0; string Action; cin >> N >> K;
-    while (N--) {
-	cin >> Action;
-	if (Action == "save") { SaveBullet = Bullet; }
-	else if (Action == "load") { Bullet = SaveBullet; }
-	else if (Action == "shoot") { Bullet--; }
-	else if (Action == "ammo") { Bullet += K; }
-	cout << Bullet << '\n';
-    }
-    return 0;
+	cin.tie(NULL);
+	int N, K, Bullet = 0, SaveBullet = 0; string Action; cin >> N >> K;
+	while (N--) {
+		cin >> Action;
+		if (Action == "save") { SaveBullet = Bullet; }
+		else if (Action == "load") { Bullet = SaveBullet; }
+		else if (Action == "shoot") { Bullet--; }
+		else if (Action == "ammo") { Bullet += K; }
+		cout << Bullet << '\n';
+	}
+	return 0;
 }
