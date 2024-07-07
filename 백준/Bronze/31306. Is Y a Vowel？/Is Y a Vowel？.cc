5343f8f1a56c46db3c000982390dff4b26929202
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-07-07 16:49:44 +0900

    [Bronze IV] Title: Is Y a Vowel?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.cc
+++ 백준/Bronze/31306. Is Y a Vowel？/Is Y a Vowel？.cc
@@ -4,13 +4,12 @@ using namespace std;
 int Arr[26], Count = 0;
 
 int main() {
-	char S[51]; cin >> S;
+	char S[51], Index[] = "aeiou"; cin >> S;
 	for (const char& C : S) {
 		if (C < 97 || 122 < C) { break; }
 		Arr[C - 97]++;
 	}
-	Count += Arr[0]; Count += Arr[4]; Count += Arr[8];
-	Count += Arr[14]; Count += Arr[20];
+	for (const char& C : Index) { Count += Arr[C - 97]; }
 	cout << Count << ' ' << Count + Arr[24];
 	return 0;
 }
