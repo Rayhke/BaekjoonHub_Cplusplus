4b054077785967f0a21c1ed18351d81571dacb90
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-08-20 17:04:54 +0900

    [Bronze V] Title: 2033년 밈 투표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string Pledge[] = {
+	"Never gonna give you up",
+	"Never gonna let you down",
+	"Never gonna run around and desert you",
+	"Never gonna make you cry",
+	"Never gonna say goodbye",
+	"Never gonna tell a lie and hurt you",
+	"Never gonna stop"
+};
+
+int main() {
+	bool pass; int N; string In; cin >> N; cin.ignore();
+	while (N--) {
+		getline(cin, In); pass = 0;
+		for (string pledge : Pledge) {
+			if (In == pledge) { pass = 1; break; }
+		}
+		if (!pass) { cout << "Yes"; return 0; }
+	}
+	cout << "No";
+	return 0;
+}
