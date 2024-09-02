8dffb7a417fea61bc50259046e6070b70a0056ef
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-09-02 22:28:30 +0900

    [Bronze II] Title: 이산수학 과제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14723. 이산수학 과제/이산수학 과제.cc
+++ 백준/Bronze/14723. 이산수학 과제/이산수학 과제.cc
@@ -2,7 +2,7 @@
 
 int main() {
 	int N, A = 1, B = 1, Max = 2; std::cin >> N;
-	for (int n = 1; n < N; n++) {
+	while (--N) {
 		if (B + 1 == Max) { Max++; A = Max - 1; B = 1; }
 		else { A--; B++; }
 	}
