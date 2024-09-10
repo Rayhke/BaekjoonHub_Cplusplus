207a972d9a0428df2863e8b770af1d8ce8946d06
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-09-11 02:12:53 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -2,6 +2,6 @@
 
 int main() {
 	int A1, A2, C, N; std::cin >> A1 >> A2 >> C >> N;
-	std::cout << ((A1 * N + A2 <= C * N) && (A1 <= C) ? 1 : 0);
+	std::cout << ((A1 * N + A2 > C * N) || (A1 > C) ? 0 : 1);
 	return 0;
 }
