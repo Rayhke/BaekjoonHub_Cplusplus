db3ad6dbfa4537b6759717b878d6de7e59442223
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-10-17 22:16:42 +0900

    [Bronze III] Title: 서울사이버대학을 다니고, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Bronze/30958. 서울사이버대학을 다니고/서울사이버대학을 다니고.cc
+++ 백준/Bronze/30958. 서울사이버대학을 다니고/서울사이버대학을 다니고.cc
@@ -5,6 +5,7 @@ using namespace std;
 int Arr[26];
 
 int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	int N, Max = 0; string S; cin >> N;
 	cin.ignore(); getline(cin, S);
 	for (int n = 0; n < N; n++) { Arr[S[n] - 97]++; }
