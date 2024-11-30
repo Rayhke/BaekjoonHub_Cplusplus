3115557b8ddda7cda31c9401cc05ef76953d4481
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2024-11-30 16:59:53 +0900

    Update Artistic Souvenir.cc

--- 백준/Bronze/30886. Artistic Souvenir/Artistic Souvenir.cc
+++ 백준/Bronze/30886. Artistic Souvenir/Artistic Souvenir.cc
@@ -2,12 +2,12 @@
 #include <cmath>
 using namespace std;
 
-long double Area, Side, PI = acos(-1);
+long double Area, Radius, Side, PI = acos(-1);
 
 int main() {
 	cin >> Area;
-	Side = sqrtl(Area / PI);
-	Side = Side * 2 + 2;
+	Radius = sqrtl(Area / PI);
+	Side = Radius * 2 + 2;
 	printf("%0.10Lf", powl(Side, 2));
 	return 0;
 }
