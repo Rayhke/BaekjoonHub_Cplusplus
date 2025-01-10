5040aacfe031562b358ce86df18fd524025443f7
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2025-01-10 22:47:32 +0900

    [Bronze IV] Title: 체스 초보 브실이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int Point[26];
+
+int main() {
+	int P1 = 0, P2 = 0; char S[9];
+	Point[15] = 1; Point[13] = 3; Point[1] = 3;
+	Point[17] = 5; Point[16] = 9;
+	for (int n = 0; n < 8; n++) {
+		cin >> S;
+		for (int n = 0; n < 8; n++) {
+			if (S[n] == '.') { continue; }
+			S[n] < 91 ? P1 += Point[S[n] - 65] : P2 += Point[S[n] - 97];
+		}
+	}
+	cout << P1 - P2;
+	return 0;
+}
