dd2e5a466b51d7f151cb14939848ec06ad3c4911
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2025-04-23 01:00:49 +0900

    [Bronze IV] Title: 선물, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Bronze/28281. 선물/선물.cc
+++ 백준/Bronze/28281. 선물/선물.cc
@@ -4,7 +4,7 @@ using namespace std;
 int A[100000], Min = 1e9, Index = 1;
 
 int main() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false); cin.tie(NULL);
 	int N, X; cin >> N >> X;
 	for (int n = 0; n < N; n++) { cin >> A[n]; }
 	for (int n = 1; n < N; n++) {
