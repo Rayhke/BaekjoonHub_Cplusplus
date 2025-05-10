7e64b1bf8ad680e31b80d518d2c17184896c46d9
Hardtack-Meat <94730654+Rayhke@users.noreply.github.com>
2025-05-10 22:36:08 +0900

    [Bronze IV] Title: Animal King Election, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17598. Animal King Election/Animal King Election.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	int N = 0; std::string S;
+	for (int n = 0; n < 9; n++) {
+		std::cin >> S;
+		S[0] == 'L' ? N++ : N;
+	}
+	std::cout << (N > 4 ? "Lion" : "Tiger");
+	return 0;
+}
