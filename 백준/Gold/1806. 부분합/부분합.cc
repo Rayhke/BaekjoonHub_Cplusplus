25ddd3d84654b5e7b099ad4eef0132eec1e9cb20
Hardtack-Meat-Cat <nink2458@naver.com>
2025-08-29 07:05:11 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int Arr[100000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	int N, S, L = 0, Min = 1e9, Sum = 0; cin >> N >> S;
+	for (int n = 0; n < N; n++) {
+		cin >> Arr[n];
+	}
+	for (int R = 0; R < N; R++) {
+		Sum += Arr[R];
+		while (Sum >= S) {
+			Min = min(R - L + 1, Min);
+			Sum -= Arr[L++];
+		}
+	}
+	cout << (Min != 1e9 ? Min : 0);
+	return 0;
+}
