7993dfe8c6b091118f50cf9912df38877e7ec8e4
Hardtack-Meat-Cat <nink2458@naver.com>
2025-09-21 07:08:26 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int Min = 1e9, Index[3][2] = { { 1, 2 }, { 0, 2 }, { 0, 1 } };
-
 int main() {
-	int N, In[1000][3] = { 0 }, DP[1000][3] = { 0 }; cin >> N;
+	int N, L, R, Min = 1e9; cin >> N;
+	int In[1000][3] = { 0 }, DP[1000][3] = { 0 };
 	for (int n = 0; n < N; n++) {
 		for (int& in : In[n]) { cin >> in; }
 	}
@@ -13,7 +12,9 @@ int main() {
 	}
 	for (int n = 1; n < N; n++) {
 		for (int m = 0; m < 3; m++) {
-			DP[n][m] = min(DP[n - 1][Index[m][0]], DP[n - 1][Index[m][1]]) + In[n][m];
+			L = m - 1 < 0 ? 2 : m - 1;
+			R = m + 1 > 2 ? 0 : m + 1;
+			DP[n][m] = min(DP[n - 1][L], DP[n - 1][R]) + In[n][m];
 		}
 	}
 	for (int& dp : DP[N - 1]) { Min = min(dp, Min); }
