fb040b0cee5cb6d7f63cfd7dce362c90aee44ee8
Hardtack-Meat-Cat <nink2458@naver.com>
2025-10-04 14:56:15 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+using PAIR = pair<int, int>;
+
+PAIR Job[15];
+int DP[16];
+
+int main() {
+	int N, T, P; cin >> N;
+	for (int n = 0; n < N; n++) {
+		cin >> T >> P; Job[n] = { T,P };
+	}
+	for (int n = 0; n < N; n++) {
+		T = Job[n].first + n;
+		P = Job[n].second;
+		if (n > 0) { DP[n] = max(DP[n - 1], DP[n]); }
+		if (T < N + 1) {
+			DP[T] = max(DP[n] + P, DP[T]);
+		}
+	}
+	cout << max(DP[N - 1], DP[N]);
+	return 0;
+}
