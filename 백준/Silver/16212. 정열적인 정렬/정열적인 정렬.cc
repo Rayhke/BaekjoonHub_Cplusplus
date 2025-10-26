21fd8a73bfd6832d0a0fc6b99bf9f2c8c70613cf
Hardtack-Meat-Cat <nink2458@naver.com>
2025-10-26 17:49:01 +0900

    [Silver V] Title: 정열적인 정렬, Time: 120 ms, Memory: 3852 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int N, In[500000]; cin >> N;
+	for (int n = 0; n < N; n++) { cin >> In[n]; }
+	sort(In, In + N);
+	for (int n = 0; n < N; n++) { cout << In[n] << ' '; }
+	return 0;
+}
